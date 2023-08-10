c60d8e84a77cb1735ed35a342bdcd0bc99a9795c
rinkal.bhojani <rinkal.bhojani@ayanworks.com>
2023-08-10 15:19:18 +0530

    fix: improvements in global exception handling for apigateway and service
    Signed-off-by: rinkal.bhojani <rinkal.bhojani@ayanworks.com>

--- libs/service/base.service.ts
+++ libs/service/base.service.ts
@@ -1,6 +1,6 @@
 import { HttpException, Logger } from '@nestjs/common';
 
-import { ClientProxy } from '@nestjs/microservices';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { map } from 'rxjs/operators';
 
 export class BaseService {
@@ -24,39 +24,6 @@ export class BaseService {
         }))
       )
       .toPromise()
-      .catch((error) => {
-        this.logger.error(`catch: ${JSON.stringify(error)}`);
-        if (error && error.message) {
-          throw new HttpException(
-            {
-              status: error.statusCode,
-              error: error.message
-            },
-            error.statusCode
-          );
-        } else if (error) {
-          throw new HttpException(
-            {
-              status: error.statusCode,
-              error: error.error
-            },
-            error.statusCode
-            
-          );
-        } else {
-          this.logger.error(
-            `The error received was in an unexpected format. Returning generic 500 error... ${JSON.stringify(
-              error
-            )}`
-          );
-          throw new HttpException(
-            {
-              status: 500,
-              error: error.message
-            },
-            500
-          );
-        }
-      });
+      ;
   }
 }

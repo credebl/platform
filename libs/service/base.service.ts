041cc1e7f82813c1164b5f47dc5d3e7a8b4956e1
rinkal.bhojani <rinkal.bhojani@ayanworks.com>
2023-08-10 14:39:22 +0530

    fix: improvements in global exception handling for apigateway and service
    Signed-off-by: rinkal.bhojani <rinkal.bhojani@ayanworks.com>

--- libs/service/base.service.ts
+++ libs/service/base.service.ts
@@ -23,40 +23,6 @@ export class BaseService {
           //duration: Date.now() - startTs,
         }))
       )
-      .toPromise()
-      .catch((error) => {
-        this.logger.error(`catch: ${JSON.stringify(error)}`);
-        if (error && error.message) {
-          throw new HttpException(
-            {
-              status: error.statusCode,
-              error: error.message
-            },
-            error.statusCode
-          );
-        } else if (error) {
-          throw new HttpException(
-            {
-              status: error.statusCode,
-              error: error.error
-            },
-            error.statusCode
-            
-          );
-        } else {
-          this.logger.error(
-            `The error received was in an unexpected format. Returning generic 500 error... ${JSON.stringify(
-              error
-            )}`
-          );
-          throw new HttpException(
-            {
-              status: 500,
-              error: error.message
-            },
-            500
-          );
-        }
-      });
+      .toPromise();
   }
 }

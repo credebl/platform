94ffc465acfa01f99a61f98131559e6b48347ce3
KulkarniShashank <shashank.kulkarni@ayanworks.com>
2023-09-13 15:59:21 +0530

    Added the try catch for the error handling in the login functionality
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/user/src/user.service.ts
+++ apps/user/src/user.service.ts
@@ -58,7 +58,7 @@ export class UserService {
   async sendVerificationMail(userEmailVerificationDto: UserEmailVerificationDto): Promise<user> {
     try {
       const userDetails = await this.userRepository.checkUserExist(userEmailVerificationDto.email);
-      
+
       if (userDetails && userDetails.isEmailVerified) {
         throw new ConflictException(ResponseMessages.user.error.exists);
       }
@@ -277,7 +277,7 @@ export class UserService {
         return this.generateToken(email, decryptedPassword);
       }
 
-     return this.generateToken(email, password);
+      return this.generateToken(email, password);
     } catch (error) {
       this.logger.error(`In Login User : ${JSON.stringify(error)}`);
       throw new RpcException(error.response);
@@ -285,24 +285,28 @@ export class UserService {
   }
 
   async generateToken(email: string, password: string): Promise<object> {
-    const supaInstance = await this.supabaseService.getClient();
+    try {
+      const supaInstance = await this.supabaseService.getClient();
 
-    this.logger.error(`supaInstance::`, supaInstance);
+      this.logger.error(`supaInstance::`, supaInstance);
 
-    const { data, error } = await supaInstance.auth.signInWithPassword({
-      email,
-      password
-    });
+      const { data, error } = await supaInstance.auth.signInWithPassword({
+        email,
+        password
+      });
 
-    this.logger.error(`Supa Login Error::`, JSON.stringify(error));
+      this.logger.error(`Supa Login Error::`, JSON.stringify(error));
 
-    if (error) {
-      throw new BadRequestException(error?.message);
+      if (error) {
+        throw new BadRequestException(error?.message);
+      }
+
+      const token = data?.session;
+
+      return token;
+    } catch (error) {
+      throw new RpcException(error.response);
     }
-
-    const token = data?.session;
-
-    return token;
   }
 
   async getProfile(payload: { id }): Promise<object> {

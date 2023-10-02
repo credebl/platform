cc4b82c63389180def448910c00c779ac20e7dc7
tipusinghaw <126460794+tipusinghaw@users.noreply.github.com>
2023-10-02 17:33:45 +0530

    feat: ecosystem ms setup (#104)
    * worked on the master table json file and .env sample refractoring
    
    Signed-off-by: @nishad.shirsat <nishad.shirsat@ayanworks.com>
    
    * merge dev branch to main (#77)
    
    * fix: Changed the passkey approch
    
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>
    
    * feat/fix: Implemented Add passkey for existing users
    
    Signed-off-by: tipusinghaw <tipu.singh@ayanworks.com>
    
    * feat:implemented add passke
    
    Signed-off-by: tipusinghaw <tipu.singh@ayanworks.com>
    
    * fix: login error message
    
    Signed-off-by: tipusinghaw <tipu.singh@ayanworks.com>
    
    ---------
    
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>
    Signed-off-by: tipusinghaw <tipu.singh@ayanworks.com>
    
    * Included credebl-master-table json file in the .gitignore
    
    Signed-off-by: @nishad.shirsat <nishad.shirsat@ayanworks.com>
    
    * Create ecosystem monorepo
    
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>
    
    * feat: Implemented ecosystem microservice
    
    Signed-off-by: tipusinghaw <tipu.singh@ayanworks.com>
    
    * feat: changed controller name
    
    Signed-off-by: tipusinghaw <tipu.singh@ayanworks.com>
    
    ---------
    
    Signed-off-by: tipusinghaw <tipu.singh@ayanworks.com>
    Signed-off-by: @nishad.shirsat <nishad.shirsat@ayanworks.com>
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>
    Co-authored-by: Nishad Shirsat <103021375+nishad-ayanworks@users.noreply.github.com>
    Co-authored-by: @nishad.shirsat <nishad.shirsat@ayanworks.com>
    Co-authored-by: Shashank Kulkarni <44693969+KulkarniShashank@users.noreply.github.com>
    Co-authored-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>
    Signed-off-by: tipusinghaw <tipu.singh@ayanworks.com>

---
+++ apps/ecosystem/src/ecosystem.service.ts
@@ -0,0 +1,20 @@
+// eslint-disable-next-line camelcase
+import { Injectable} from '@nestjs/common';
+
+@Injectable()
+export class EcosystemService {
+  constructor(  
+  ) { }
+
+  /**
+   *
+   * @param registerOrgDto
+   * @returns
+   */
+
+  // eslint-disable-next-line camelcase
+  async createEcosystem():Promise<string> {
+    return "test";
+  }
+
+}

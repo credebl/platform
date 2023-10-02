6ac7a2284ca8002a6014a463fa19be4786f897c5
KulkarniShashank <shashank.kulkarni@ayanworks.com>
2023-10-02 11:34:13 +0530

    Added get all ledgers API in platform module
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/ledger/src/ledger.controller.ts
+++ apps/ledger/src/ledger.controller.ts
@@ -1,8 +1,14 @@
 import { Controller } from '@nestjs/common';
 import { LedgerService } from './ledger.service';
+import { MessagePattern } from '@nestjs/microservices';
+import { ledgers } from '@prisma/client';
 
 @Controller()
 export class LedgerController {
-  constructor(private readonly ledgerService: LedgerService) {}
+  constructor(private readonly ledgerService: LedgerService) { }
 
+  @MessagePattern({ cmd: 'get-all-ledgers' })
+  async getAllLedgers(): Promise<ledgers[]> {
+    return this.ledgerService.getAllLedgers();
+  }
 }
--- apps/ledger/src/ledger.service.ts
+++ apps/ledger/src/ledger.service.ts
@@ -1,5 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { BaseService } from 'libs/service/base.service';
+import { LedgerRepository } from './repositories/ledger.repository';
+import { RpcException } from '@nestjs/microservices';
+import { ledgers } from '@prisma/client';
+import { ResponseMessages } from '@credebl/common/response-messages';
 
 @Injectable()
-export class LedgerService {
+export class LedgerService extends BaseService {
+
+    constructor(
+        private readonly ledgerRepository: LedgerRepository
+    ) {
+        super('LedgerService');
+    }
+
+    async getAllLedgers(): Promise<ledgers[]> {
+        try {
+            const getAllLedgerDetails = await this.ledgerRepository.getAllLedgers();
+
+            if (!getAllLedgerDetails) {
+                throw new NotFoundException(ResponseMessages.ledger.error.NotFound);
+            }
+
+            return getAllLedgerDetails;
+        } catch (error) {
+            this.logger.error(`Error in retrieving all ledgers: ${error}`);
+            throw new RpcException(error.response ? error.response : error);
+        }
+    }
 }

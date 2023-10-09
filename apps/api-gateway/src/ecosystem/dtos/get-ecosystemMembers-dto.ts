f86ce0182af3cfdc93ca694bba1cb5ddefd16f0c
bhavanakarwade <bhavana.karwade@ayanworks.com>
2023-10-09 19:21:19 +0530

    resolved sonarcloud checks
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/api-gateway/src/ecosystem/dtos/get-ecosystemMembers-dto.ts
+++ apps/api-gateway/src/ecosystem/dtos/get-ecosystemMembers-dto.ts
@@ -8,11 +8,6 @@ export class GetAllEcosystemMembersDto {
 
     @ApiProperty({ required: false })
     @IsOptional()
-    @Type(() => String)
-    search = '';
-
-    @ApiProperty({ required: false })
-    @IsOptional()
     @Type(() => Number)
     @Transform(({ value }) => toNumber(value))
     pageNumber = 1;
@@ -22,5 +17,11 @@ export class GetAllEcosystemMembersDto {
     @Type(() => Number)
     @Transform(({ value }) => toNumber(value))
     pageSize = 10;
+
+
+    @ApiProperty({ required: false })
+    @IsOptional()
+    @Type(() => String)
+    search = '';
 
 }

bdeed0f10e563b78c6e069e77225e4e8d3fe97e2
bhavanakarwade <bhavana.karwade@ayanworks.com>
2023-10-18 15:46:32 +0530

    refactor: send ecosystem invitations link
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/ecosystem/src/ecosystem.service.ts
+++ apps/ecosystem/src/ecosystem.service.ts
@@ -17,7 +17,7 @@ import { CreateEcosystem, CredDefMessage, RequestCredDeffEndorsement, RequestSch
 import { GetEndorsementsPayload } from '../interfaces/endorsements.interface';
 import { CommonConstants } from '@credebl/common/common.constant';
 // eslint-disable-next-line camelcase
-import { credential_definition, org_agents, platform_config, schema } from '@prisma/client';
+import { credential_definition, org_agents, platform_config, schema, user } from '@prisma/client';
 
 
 @Injectable()
@@ -158,6 +158,8 @@ export class EcosystemService {
         const { email } = invitation;
 
         const isUserExist = await this.checkUserExistInPlatform(email);
+
+        const isInvitationExist = await this.checkInvitationExist(email, ecosystemId);
 
         const isInvitationExist = await this.checkInvitationExist(email, ecosystemId);
         if (!isInvitationExist && userEmail !== invitation.email) {

ede6da9a7072be5cca1ba92165dbac232bbba511
bhavanakarwade <bhavana.karwade@ayanworks.com>
2023-10-26 15:16:30 +0530

    added class to image tag
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/verification/templates/out-of-band-verification.template.ts
+++ apps/verification/templates/out-of-band-verification.template.ts
@@ -12,13 +12,10 @@ export class OutOfBandVerification {
             </head>
             
             <body style="margin: 0px; padding:0px; background-color:#F9F9F9;">
-                <div style="margin: auto; width: 40%; padding: 20px 30px; background-color: #FFFFFF; display:block;">
+                <div style="margin: auto; max-width: 450px; padding: 20px 30px; background-color: #FFFFFF; display:block;">
                     <div style="display: block; text-align:center;">
-                        <img src="${process.env.API_GATEWAY_PROTOCOL}://${process.env.API_ENDPOINT}/CREDEBL_Logo.png" alt="Credebl Logo">
+                    <img src="${process.env.API_GATEWAY_PROTOCOL}://${process.env.API_ENDPOINT}/${process.env.PLATFORM_LOGO}" alt="CREDEBL logo" style="max-width:100px" width="100%" height="fit-content" class="CToWUd" data-bit="iit">
                     </div>
-                    <div style="display: block; text-align:center;">
-                        <img src="${process.env.API_GATEWAY_PROTOCOL}://${process.env.API_ENDPOINT}/verification-image.png" alt="verification Image" style="max-width:100%;">
-                        </div>
                     <div style="font-family: Montserrat; font-style: normal; font-weight: 500;
                     font-size: 15px; line-height: 24px;color: #5E5873;">
                         <p style="margin-top:0px">
@@ -37,7 +34,7 @@ export class OutOfBandVerification {
                             </ul>
                             Should you encounter any difficulties or have inquiries, our dedicated support team is available to assist you. Feel free to reach out.
                          </p>
-                         <img src="${process.env.API_GATEWAY_PROTOCOL}://${process.env.API_ENDPOINT}/verification/oob/qr/${verificationQrCode}" alt="QR Code" class="CToWUd" width="200" height="200">
+                         <img src="${process.env.API_GATEWAY_PROTOCOL}://${process.env.API_ENDPOINT}/verification/oob/qr/${verificationQrCode}" alt="QR Code" class="CToWUd" width="200" height="200" data-bit="iit">
                         <hr style="border-top:1px solid #e8e8e8" />
                         <footer style="padding-top: 20px;">
                             <div>

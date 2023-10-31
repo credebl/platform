97edbdeb229909ecabbf00977df1a1f13cd1ea29
Nishad Shirsat <103021375+nishad-ayanworks@users.noreply.github.com>
2023-10-31 20:17:40 +0530

    Fixed the issue of endorsement request for the specific ecosystem (#216)
    Signed-off-by: Nishad <nishad.shirsat@ayanworks.com>
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/ecosystem/src/ecosystem.service.ts
+++ apps/ecosystem/src/ecosystem.service.ts
@@ -364,7 +364,7 @@ export class EcosystemService {
         this.ecosystemRepository.getAgentDetails(orgId),
         this.ecosystemRepository.getPlatformConfigDetails(),
         this.ecosystemRepository.getAgentDetails(Number(getEcosystemLeadDetails.orgId)),
-        this.ecosystemRepository.getEcosystemOrgDetailsbyId(String(orgId))
+        this.ecosystemRepository.getEcosystemOrgDetailsbyId(String(orgId), ecosystemId)
       ]);
 
       if (0 !== schemaRequestExist.length) {
@@ -435,7 +435,7 @@ export class EcosystemService {
         this.ecosystemRepository.getAgentDetails(orgId),
         this.ecosystemRepository.getPlatformConfigDetails(),
         this.ecosystemRepository.getAgentDetails(Number(getEcosystemLeadDetails.orgId)),
-        this.ecosystemRepository.getEcosystemOrgDetailsbyId(String(orgId))
+        this.ecosystemRepository.getEcosystemOrgDetailsbyId(String(orgId), ecosystemId)
       ]);
 
       if (0 !== credDefRequestExist.length) {

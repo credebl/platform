fb1efdfa06a99e33e5b7eb8835deb48f4aaecebe
pallavicoder <pallavi.ghule@ayanworks.com>
2023-11-03 13:25:36 +0530

    refact:enum constant conversion from number to string
    Signed-off-by: pallavicoder <pallavi.ghule@ayanworks.com>
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- libs/enum/src/enum.ts
+++ libs/enum/src/enum.ts
@@ -4,8 +4,8 @@ export enum SortValue {
 }
 
 export enum AgentType {
-    AFJ = 1,
-    ACAPY = 2
+    AFJ = '1',
+    ACAPY = '2'
 }
 export enum Invitation {
     ACCEPTED = 'accepted',
@@ -34,6 +34,6 @@ export enum EndorserTransactionType{
 }
 
 export enum OrgAgentType {
-    DEDICATED = 1,
-    SHARED = 2
+    DEDICATED = `1`,
+    SHARED = `2`
 }

5be6981823295ca1551fcf5a6751e4e2c2fa4b6a
pallavicoder <pallavi.ghule@ayanworks.com>
2023-11-03 13:13:34 +0530

    refact:converting all orgId userId ,EcosystemId from number to string in Ecosystem  micoservice
    Signed-off-by: pallavicoder <pallavi.ghule@ayanworks.com>
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/ecosystem/src/ecosystem.repository.ts
+++ apps/ecosystem/src/ecosystem.repository.ts
@@ -446,6 +446,7 @@ export class EcosystemRepository {
     // eslint-disable-next-line camelcase
   ): Promise<ecosystem_invitations> {
     try {
+     
       return this.prisma.ecosystem_invitations.create({
         data: {
           email,
@@ -709,14 +710,14 @@ export class EcosystemRepository {
   * @returns Get getAgentEndPoint details
   */
   // eslint-disable-next-line camelcase
-  async getAgentDetails(orgId: number): Promise<org_agents> {
+  async getAgentDetails(orgId: string): Promise<org_agents> {
     try {
       if (!orgId) {
         throw new InternalServerErrorException(ResponseMessages.ecosystem.error.invalidOrgId);
       }
       const agentDetails = await this.prisma.org_agents.findFirst({
         where: {
-          orgId
+          orgId:orgId.toString()
         }
       });
       return agentDetails;
@@ -1012,10 +1013,10 @@ export class EcosystemRepository {
           attributes,
           schemaLedgerId,
           issuerId,
-          createdBy: Number(createdBy),
-          lastChangedBy: Number(lastChangedBy),
+          createdBy,
+          lastChangedBy,
           publisherDid,
-          orgId: Number(orgId),
+          orgId: String(orgId),
           ledgerId
         }
       });
@@ -1036,8 +1037,8 @@ export class EcosystemRepository {
           tag,
           credentialDefinitionId,
           revocable,
-          createdBy: Number(createdBy),
-          orgId: Number(orgId),
+          createdBy,
+          orgId,
           schemaId
         }
       });

be659ae0e2e1b9c1a05891d48e24044498eb59c3
KulkarniShashank <shashank.kulkarni@ayanworks.com>
2023-11-21 16:51:36 +0530

    fix: error handling on the bulk issuance
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/issuance/src/issuance.service.ts
+++ apps/issuance/src/issuance.service.ts
@@ -43,7 +43,7 @@ export class IssuanceService {
     @InjectQueue('bulk-issuance') private bulkIssuanceQueue: Queue
   ) { }
 
- 
+
   async sendCredentialCreateOffer(orgId: number, user: IUserRequest, credentialDefinitionId: string, comment: string, connectionId: string, attributes: object[]): Promise<string> {
     try {
       const agentDetails = await this.issuanceRepository.getAgentEndPoint(orgId);
@@ -877,7 +877,7 @@ export class IssuanceService {
       autoConnect: true,
       transports: ['websocket']
     });
-  
+
     const fileUploadData: FileUploadData = {
       fileUpload: '',
       fileRow: '',
@@ -920,7 +920,7 @@ export class IssuanceService {
       }
     } catch (error) {
       this.logger.error(
-        `error in issuanceBulkCredential for data ${jobDetails} : ${error}`
+        `error in issuanceBulkCredential for data ${JSON.stringify(jobDetails)} : ${JSON.stringify(error)}`
       );
       fileUploadData.isError = true;
       fileUploadData.error = error.message;
@@ -949,6 +949,7 @@ export class IssuanceService {
       }
     } catch (error) {
       this.logger.error(`Error completing bulk issuance process: ${error}`);
+      throw error;
     }
 
   }

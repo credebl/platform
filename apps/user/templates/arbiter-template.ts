2c1f742b339b935aef102319185cff4e4766b501
bhavanakarwade <bhavana.karwade@ayanworks.com>
2023-11-22 20:55:54 +0530

    refactor: user certificate templates
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/user/templates/arbiter-template.ts
+++ apps/user/templates/arbiter-template.ts
@@ -7,19 +7,23 @@ export class ArbiterTemplate {
 
   async getArbiterTemplate(attributes: Attribute[]): Promise<string> {
     try {
-      const [name, country, issuedBy] = await Promise.all(attributes).then((attributes) => {
+      const [name, issuedBy] = await Promise.all(attributes).then((attributes) => {
         const name = this.findAttributeByName(attributes, 'full_name')?.full_name ?? '';
-        const country = this.findAttributeByName(attributes, 'country')?.country ?? '';
         const issuedBy = this.findAttributeByName(attributes, 'issued_by')?.issued_by ?? '';
-        return [name, country, issuedBy];
+        return [name, issuedBy];
       });
       return `<!DOCTYPE html>
       <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="preconnect" href="https://fonts.googleapis.com">
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-      <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap" rel="stylesheet">
+      
         <style>
           body {
             margin: 0;
@@ -55,14 +59,19 @@ export class ArbiterTemplate {
       <img id="backgroundImage" src="https://credebl-dev-user-certificate.s3.ap-south-1.amazonaws.com/certificates/background_image.svg" alt="background" 
         style="height: 1000px; width: 770px;"
         />
-        <div id="textOverlay" style="width: 760px; height: 450px;">
+        <div id="textOverlay" style="width: 640px; height: 690px;">
           <div>
               <p style="font-size: 50px; font-family: Inter; margin: 0;">CERTIFICATE</p>
               <p style="font-size: 22px; font-family: Inter; margin: 0;"> OF RECOGNITION</p>
           </div>
           
           <p style="font-size: 15px; font-family: Inter;margin: 15; margin-top: 15px;">IS PROUDLY PRESENTED TO</p>
-          <p style="font-size: 35px; font-style: italic; margin: 0">${name}</p>
+          <p style="font-size: 45px; font-family: 'Dancing Script', cursive;
+          background: linear-gradient(90deg, #B9752B 2.4%, #F8D675 29.13%, #CD953F 52.41%, #F6D373 75.07%, #BF7E32 105.84%);
+          background-clip: text;
+          -webkit-background-clip: text;
+          -webkit-text-fill-color: transparent;
+          margin: 15px;">${name}</p>
           
           <span >
               <p style="font-size: 16px; " >has served as an Arbiter at the
@@ -77,8 +86,8 @@ export class ArbiterTemplate {
               <p style="font-size: 14px; margin: 0;">played a crucial role in maintaining the credibility of the competition.</p>
       
           </p>
-          <div style="font-family: Inter; font-weight: bold; font-size: 12px;">Date: 24, 25, 26 November 2023 | Place: Cidco Exhibition Centre, Navi Mumbai, ${country}</div>
-          <div style="font-family: Inter; font-weight: bold; font-size: 12px; margin-top:316px">Blockchain-based certificate issued using "credebl.id", by Blockster Labs Pvt. Ltd.</div>
+          <div style="font-family: Inter; font-weight: bold; font-size: 12px;">Date: 24, 25, 26 November 2023 | Place: Cidco Exhibition Centre, Navi Mumbai, India</div>
+          <div style="font-family: Inter;font-weight: bold;font-size: 12px;position: absolute;bottom: 6px;left: 50%;transform: translateX(-50%);width: 100%;">Blockchain-based certificate issued using "credebl.id", by Blockster Labs Pvt. Ltd.</div>
         </div>
       </div>
       

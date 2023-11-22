8d3da91ad4f11ed8902e7b968cfdfa62db4a6e1b
tipusinghaw <126460794+tipusinghaw@users.noreply.github.com>
2023-11-22 19:15:46 +0530

    feat: added separate socket pattern for retry (#294)
    * fix: seperate socket for retry
    
    Signed-off-by: tipusinghaw <tipu.singh@ayanworks.com>
    
    * fix: changed error message
    
    Signed-off-by: tipusinghaw <tipu.singh@ayanworks.com>
    
    ---------
    
    Signed-off-by: tipusinghaw <tipu.singh@ayanworks.com>
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/api-gateway/src/authz/socket.gateway.ts
+++ apps/api-gateway/src/authz/socket.gateway.ts
@@ -118,4 +118,12 @@ export class SocketGateway implements OnGatewayConnection {
       .to(payload.clientId)
       .emit('error-in-bulk-issuance-process', payload.error);
   }
+
+  @SubscribeMessage('bulk-issuance-process-retry-completed')
+  async handleBulkIssuanceRetry(client:string, payload: ISocketInterface): Promise<void> {
+    this.logger.log(`bulk-issuance-process-retry-completed ${payload.clientId}`);
+    this.server
+      .to(payload.clientId)
+      .emit('bulk-issuance-process-retry-completed');
+  }
 }

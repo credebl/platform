257b3c855044abad019caa138262a0a7bf118fc1
Nishad <nishad.shirsat@ayanworks.com>
2023-11-25 10:33:57 +0530

    refactored certificate templates
    Signed-off-by: Nishad <nishad.shirsat@ayanworks.com>
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/user/templates/winner-template.ts
+++ apps/user/templates/winner-template.ts
@@ -87,7 +87,7 @@ export class WinnerTemplate {
               <p style="font-size: 14px; margin: 0;">exceptional memory skills demonstrated during the competition.</p>
           </p>
           <div style="font-family: Inter; font-weight: bold; font-size: 12px;">Date: 24, 25, 26 November 2023 | Place: Cidco Exhibition Centre, Navi Mumbai, India</div>
-          <div style="font-family: Inter;font-weight: bold;font-size: 12px;position: absolute;bottom: 6px;left: 50%;transform: translateX(-50%);width: 100%;">Blockchain-based certificate issued using "credebl.id", by Blockster Labs Pvt. Ltd.</div>
+          <div style="font-family: Inter;font-weight: bold;font-size: 12px;position: absolute;bottom: 6px;left: 50%;transform: translateX(-50%);width: 100%;">Blockchain-based certificate issued using credebl.id, by Blockster Labs Pvt. Ltd.</div>
         </div>
       </div>
       

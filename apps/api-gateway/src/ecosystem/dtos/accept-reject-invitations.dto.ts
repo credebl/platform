5ea114b637fee455a0c7e733c1f769211daeb3f7
Nishad <nishad.shirsat@ayanworks.com>
2023-11-29 11:41:18 +0530

    worked on the ecosystem organization model mapping
    Signed-off-by: Nishad <nishad.shirsat@ayanworks.com>
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/api-gateway/src/ecosystem/dtos/accept-reject-invitations.dto.ts
+++ apps/api-gateway/src/ecosystem/dtos/accept-reject-invitations.dto.ts
@@ -1,4 +1,4 @@
-import { IsEnum, IsNotEmpty, IsString, MaxLength, MinLength} from 'class-validator';
+import { IsEnum, IsNotEmpty} from 'class-validator';
 
 import { ApiProperty } from '@nestjs/swagger';
 import { Invitation } from '@credebl/enum/enum';
@@ -10,20 +10,6 @@ export class AcceptRejectEcosystemInvitationDto {
     ecosystemId: string;
     invitationId: string;
     orgId: string;
-
-    @ApiProperty()
-    @Transform(({ value }) => trim(value))
-    @IsNotEmpty({ message: 'organization name is required.' })
-    @MinLength(2, { message: 'organization name must be at least 2 characters.' })
-    @MaxLength(50, { message: 'organization name must be at most 50 characters.' })
-    @IsString({ message: 'organization name must be in string format.' })
-    orgName: string;
-
-    @ApiProperty()
-    @Transform(({ value }) => trim(value))
-    @IsNotEmpty({ message: 'organization did is required.' })
-    @IsString({ message: 'organization did must be in string format.' })
-    orgDid: string;
 
     @ApiProperty({
         enum: [Invitation.ACCEPTED, Invitation.REJECTED]

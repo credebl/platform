5429d1a75b17931097557220b516187dbc22772e
bhavanakarwade <bhavana.karwade@ayanworks.com>
2023-12-23 16:05:34 +0530

    refactor: get all connections api
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>

--- apps/api-gateway/src/connection/dtos/get-all-connections.dto.ts
+++ apps/api-gateway/src/connection/dtos/get-all-connections.dto.ts
@@ -1,27 +1,41 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { Type } from "class-transformer";
-import { IsOptional } from "class-validator";
+import { Transform, Type } from "class-transformer";
+import { IsEnum, IsOptional } from "class-validator";
 import { SortValue } from "../../enum";
+import { trim } from "@credebl/common/cast.helper";
+import { SortFields } from "apps/connection/src/enum/connection.enum";
 
 export class GetAllConnectionsDto {
+    
     @ApiProperty({ required: false })
     @IsOptional()
+    @Transform(({ value }) => trim(value))
     @Type(() => String)
     searchByText: string = '';
 
-    @ApiProperty({ required: false })
+    @ApiProperty({ required: false, example: '1' })
     @IsOptional()
     pageNumber: number = 1;
 
-    @ApiProperty({ required: false })
+    @ApiProperty({ required: false, example: '10' })
     @IsOptional()
     pageSize: number = 10;
 
-    @ApiProperty({ required: false })
+    @ApiProperty({
+        required: false
+    })
+    @Transform(({ value }) => trim(value))
     @IsOptional()
-    sorting: string = 'id';
+    @IsEnum(SortFields)
+    sortField: string = SortFields.CREATED_DATE_TIME;
 
-    @ApiProperty({ required: false })
+    @ApiProperty({
+        enum: [SortValue.DESC, SortValue.ASC],
+        required: false
+    })
+    @Transform(({ value }) => trim(value))
     @IsOptional()
-    sortByValue: string = SortValue.DESC;
+    @IsEnum(SortValue)
+    sortBy: string = SortValue.DESC;
 }
+

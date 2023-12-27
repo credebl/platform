55f4e68639484bde0f03275b7fc0cafa953d9d90
KulkarniShashank <shashank.kulkarni@ayanworks.com>
2023-12-27 12:18:56 +0530

    Changes and solved the issue for the get org agent details related function
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/agent-service/src/repositories/agent-service.repository.ts
+++ apps/agent-service/src/repositories/agent-service.repository.ts
@@ -330,5 +330,24 @@ export class AgentServiceRepository {
       this.logger.error(`[getAgentType] - get agent type details: ${JSON.stringify(error)}`);
       throw error;
     }
-  }
+
+    /**
+   * Get agent details
+   * @param orgId 
+   * @returns Agent health details
+   */
+    // eslint-disable-next-line camelcase
+    async getOrgAgentDetails(orgId: string): Promise<org_agents> {
+        try {
+            const oranizationAgentDetails = await this.prisma.org_agents.findFirstOrThrow({
+                where: {
+                    orgId
+                }
+            });
+            return oranizationAgentDetails;
+        } catch (error) {
+            this.logger.error(`[getOrgAgentDetails] - get org agent health details: ${JSON.stringify(error)}`);
+            throw error;
+        }
+    }
 }

7e77f218549245fc8c629aeeb31833be476739d8
bhavanakarwade <bhavana.karwade@ayanworks.com>
2023-12-27 17:36:53 +0530

    refactored response
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/connection/src/interfaces/connection.interfaces.ts
+++ apps/connection/src/interfaces/connection.interfaces.ts
@@ -116,17 +116,3 @@ export interface IConnectionSearchCriteria {
   searchByText: string;
   user: IUserRequestInterface
 }
-export interface IConnectionDetailsById {
-  id: string;
-  createdAt: string;
-  did: string;
-  theirDid: string;
-  theirLabel: string;
-  state: string;
-  role: string;
-  autoAcceptConnection: boolean;
-  threadId: string;
-  protocol: string;
-  outOfBandId: string;
-  updatedAt: string;
-}

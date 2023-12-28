24a517995dd3eae522efede2b28921c8cdc760f3
bhavanakarwade <bhavana.karwade@ayanworks.com>
2023-12-28 13:59:12 +0530

    refactor: get all proof presentations api
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/verification/src/repositories/verification.repository.ts
+++ apps/verification/src/repositories/verification.repository.ts
@@ -3,9 +3,10 @@ import { PrismaService } from '@credebl/prisma-service';
 import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 // eslint-disable-next-line camelcase
 import { org_agents, organisation, platform_config, presentations } from '@prisma/client';
-import { ProofPresentationPayload } from '../interfaces/verification.interface';
+import { IProofRequestsSearchCriteria, ProofPresentationPayload } from '../interfaces/verification.interface';
 import { IUserRequest } from '@credebl/user-request/user-request.interface';
-import { IProofRequestsSearchCriteria } from 'apps/api-gateway/src/verification/interfaces/verification.interface';
+import { IProofsPresentationsListCount } from '@credebl/common/interfaces/verification.interface';
+import { SortValue } from '@credebl/enum/enum';
 
 @Injectable()
 export class VerificationRepository {
@@ -57,23 +58,15 @@ export class VerificationRepository {
     user: IUserRequest,
     orgId: string,
     proofRequestsSearchCriteria: IProofRequestsSearchCriteria
-  ): Promise<{
-    proofRequestsCount: number;
-    proofRequestsList: {
-      createDateTime: Date;
-      createdBy: string;
-      connectionId: string;
-      state: string;
-      orgId: string;
-    }[];
-  }> {
+  ): Promise<IProofsPresentationsListCount> {
     try {
       const proofRequestsList = await this.prisma.presentations.findMany({
         where: {
           orgId,
           OR: [
             { connectionId: { contains: proofRequestsSearchCriteria.searchByText, mode: 'insensitive' } },
-            { state: { contains: proofRequestsSearchCriteria.searchByText, mode: 'insensitive' } }
+            { state: { contains: proofRequestsSearchCriteria.searchByText, mode: 'insensitive' } },
+            { presentationId: { contains: proofRequestsSearchCriteria.searchByText, mode: 'insensitive' } }
         ]
         },
         select: {
@@ -86,11 +79,9 @@ export class VerificationRepository {
           presentationId: true
         },
         orderBy: {
-          [proofRequestsSearchCriteria?.sorting || 'createDateTime']:
-            'DESC' === proofRequestsSearchCriteria?.sortByValue
-              ? 'desc'
-              : 'asc'
+          [proofRequestsSearchCriteria.sortField]: SortValue.ASC === proofRequestsSearchCriteria.sortBy ? 'asc' : 'desc' 
         },
+
         take: Number(proofRequestsSearchCriteria.pageSize),
         skip: (proofRequestsSearchCriteria.pageNumber - 1) * proofRequestsSearchCriteria.pageSize
       });
@@ -99,8 +90,9 @@ export class VerificationRepository {
           orgId,
           OR: [
             { connectionId: { contains: proofRequestsSearchCriteria.searchByText, mode: 'insensitive' } },
-            { state: { contains: proofRequestsSearchCriteria.searchByText, mode: 'insensitive' } }
-        ]
+            { state: { contains: proofRequestsSearchCriteria.searchByText, mode: 'insensitive' } },
+            { presentationId: { contains: proofRequestsSearchCriteria.searchByText, mode: 'insensitive' } }
+          ]
         }
       });
 

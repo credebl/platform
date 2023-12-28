aa5fa7e9c7625e2294c0ccebe7ef93658a4cdbe0
pranalidhanavade <pranali.dhanavade@ayanworks.com>
2023-12-28 23:20:26 +0530

    refactor:user profile api
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/api-gateway/src/user/dto/add-user.dto.ts
+++ apps/api-gateway/src/user/dto/add-user.dto.ts
@@ -36,7 +36,7 @@ export class AddUserDetailsDto {
     isPasskey?: boolean;
 }
 
-export class AddPasskeyDetailsDtoDto {
+export class AddPasskeyDetailsDto {
     @ApiProperty()
     @Transform(({ value }) => trim(value))
     @IsNotEmpty({ message: 'Password is required' })

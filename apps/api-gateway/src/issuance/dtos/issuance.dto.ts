1a1a5137dccdf508eea7536b18b069252ad53eb5
sanjay-k1910 <sanjay.khatal@ayanworks.com>
2024-01-10 15:21:57 +0530

    refactor: error for specific positions
    Signed-off-by: sanjay-k1910 <sanjay.khatal@ayanworks.com>
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/api-gateway/src/issuance/dtos/issuance.dto.ts
+++ apps/api-gateway/src/issuance/dtos/issuance.dto.ts
@@ -136,7 +136,7 @@ export class OutOfBandCredentialOfferDto {
     @ApiProperty({
         example: [
             {
-                'emailId': 'abc@example.com',
+                'emailId': 'testmail@domain.com',
                 'attributes': [
                     {
                         'value': 'string',

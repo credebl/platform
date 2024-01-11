f19772fdef76c6e907d5432fe94f514a7b4c2d82
16-karan <141734327+16-karan@users.noreply.github.com>
2024-01-11 11:23:11 +0530

    fix : pagination enhancement (#429)
    * fix: implemeted pagination for ecosystem and org
    
    Signed-off-by: karan <karan.tompe@ayanworks.com>
    
    * optimized loagic
    
    Signed-off-by: karan <karan.tompe@ayanworks.com>
    
    ---------
    
    Signed-off-by: karan <karan.tompe@ayanworks.com>
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/ecosystem/src/ecosystem.repository.ts
+++ apps/ecosystem/src/ecosystem.repository.ts
@@ -176,41 +176,6 @@ export class EcosystemRepository {
   }
   
 
-  async getEcosystemInvitationsPagination(queryObject: object, status: string, pageNumber: number, pageSize: number): Promise<object> {
-    try {
-      const result = await this.prisma.$transaction([
-        this.prisma.ecosystem_invitations.findMany({
-          where: {
-            ...queryObject,
-            status
-          },
-          include: {
-            ecosystem: true
-          },
-          take: pageSize,
-          skip: (pageNumber - 1) * pageSize,
-          orderBy: {
-            createDateTime: 'desc'
-          }
-        }),
-        this.prisma.ecosystem_invitations.count({
-          where: {
-            ...queryObject
-          }
-        })
-      ]);
-
-      const [invitations, totalCount] = result;
-      const totalPages = Math.ceil(totalCount / pageSize);
-
-      return { totalPages, invitations };
-
-    } catch (error) {
-      this.logger.error(`error: ${JSON.stringify(error)}`);
-      throw new InternalServerErrorException(error);
-    }
-  }
-
   /**
    * 
    * @param ecosystemId 

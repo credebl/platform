c0e75d479184c49822f37a6563899ad54b06e3b2
sanjay-k1910 <sanjay.khatal@ayanworks.com>
2024-01-15 12:26:46 +0530

    refactor: response messages
    Signed-off-by: sanjay-k1910 <sanjay.khatal@ayanworks.com>
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- libs/common/src/response-messages/index.ts
+++ libs/common/src/response-messages/index.ts
@@ -226,7 +226,7 @@ export const ResponseMessages = {
     issuance: {
         success: {
             create: 'Credentials offer created successfully',
-            createOOB: 'Out of band credentials offer created successfully',
+            createOOB: 'Out-of-band credentials offer created successfully',
             fetch: 'Issued Credentials details fetched successfully',
             importCSV: 'File imported sucessfully',
             previewCSV: 'File details fetched sucessfully',
@@ -242,7 +242,7 @@ export const ResponseMessages = {
             notFound: 'History not found',
             credentialOfferNotFound: 'Credential offer not found',
             invitationNotFound: 'Invitation not found',
-            unableToCreateOOBOffer: 'Unable to create out of band credential offer',
+            unableToCreateOOBOffer: 'Unable to create out-of-band credential offer',
             platformConfigNotFound: 'Platform config details not found',
             emailSend: 'Unable to send email to the user',
             previewFile: 'Error while fetching file details',

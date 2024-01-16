8de0998cbfbaeb175a75c5cd71552d5d08953181
KulkarniShashank <shashank.kulkarni@ayanworks.com>
2024-01-16 17:46:30 +0530

    fix:removed unused variables
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/api-gateway/src/issuance/issuance.controller.ts
+++ apps/api-gateway/src/issuance/issuance.controller.ts
@@ -612,16 +612,16 @@ export class IssuanceController {
     @Param('id') id: string,
     @Res() res: Response
   ): Promise<Response> {
-    const  webhookUrl = await this.issueCredentialService._getWebhookUrl(issueCredentialDto.contextCorrelationId);
+    // const  webhookUrl = await this.issueCredentialService._getWebhookUrl(issueCredentialDto.contextCorrelationId);
     
     this.logger.debug(`issueCredentialDto ::: ${JSON.stringify(issueCredentialDto)}`);
   
-    if (webhookUrl) {
-      try {
-        await this.issueCredentialService._postWebhookResponse(webhookUrl, {data:issueCredentialDto});
-    } catch (error) {
-        throw new RpcException(error.response ? error.response : error);
-    }
+    // if (webhookUrl) {
+    //   try {
+    //     await this.issueCredentialService._postWebhookResponse(webhookUrl, {data:issueCredentialDto});
+    // } catch (error) {
+    //     throw new RpcException(error.response ? error.response : error);
+    // }
   
     const getCredentialDetails = await this.issueCredentialService.getIssueCredentialWebhook(issueCredentialDto, id);
     const finalResponse: IResponseType = {
@@ -631,5 +631,4 @@ export class IssuanceController {
     };
     return res.status(HttpStatus.CREATED).json(finalResponse);
   }
-}
 }

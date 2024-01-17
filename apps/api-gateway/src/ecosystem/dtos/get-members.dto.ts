d86a83e747e20e5b91aa877955afbd874079bd2e
karan <karan.tompe@ayanworks.com>
2024-01-17 20:02:54 +0530

    pagination added
    Signed-off-by: karan <karan.tompe@ayanworks.com>
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/api-gateway/src/ecosystem/dtos/get-members.dto.ts
+++ apps/api-gateway/src/ecosystem/dtos/get-members.dto.ts
@@ -1,27 +1,42 @@
 import { Transform, Type } from 'class-transformer';
-import { toNumber } from '@credebl/common/cast.helper';
+import { trim } from '@credebl/common/cast.helper';
 
 import { ApiProperty } from '@nestjs/swagger';
-import { IsOptional } from 'class-validator';
+import { IsEnum, IsOptional } from 'class-validator';
+import { SortFields } from 'apps/connection/src/enum/connection.enum';
+import { SortValue } from '@credebl/enum/enum';
 
 export class GetAllEcosystemMembersDto {
+   
+    @ApiProperty({ required: false, example: '1' })
+    @IsOptional()
+    pageNumber: number;
+
+    @ApiProperty({ required: false, example: '10' })
+    @IsOptional()
+    pageSize: number;
 
     @ApiProperty({ required: false })
     @IsOptional()
-    @Type(() => Number)
-    @Transform(({ value }) => toNumber(value))
-    pageNumber = 1;
-
-    @ApiProperty({ required: false })
-    @IsOptional()
-    @Type(() => Number)
-    @Transform(({ value }) => toNumber(value))
-    pageSize = 10;
-
-
-    @ApiProperty({ required: false })
-    @IsOptional()
+    @Transform(({ value }) => trim(value))
     @Type(() => String)
-    search = '';
+    search: string = '';
+
+    @ApiProperty({
+        required: false
+    })
+    @Transform(({ value }) => trim(value))
+    @IsOptional()
+    @IsEnum(SortFields)
+    sortField: string = SortFields.CREATED_DATE_TIME;
+
+    @ApiProperty({
+        enum: [SortValue.DESC, SortValue.ASC],
+        required: false
+    })
+    @Transform(({ value }) => trim(value))
+    @IsOptional()
+    @IsEnum(SortValue)
+    sortBy: string = SortValue.DESC;
 
 }

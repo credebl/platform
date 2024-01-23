a7a3632ca0ab2a96c0cb88897b38e019652c41c0
pallavicoder <pallavi.ghule@ayanworks.com>
2024-01-23 16:02:51 +0530

    refactor:added type property inside webhook response
    Signed-off-by: pallavicoder <pallavi.ghule@ayanworks.com>
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/api-gateway/src/verification/verification.controller.ts
+++ apps/api-gateway/src/verification/verification.controller.ts
@@ -281,6 +281,7 @@ export class VerificationController {
         @Body() proofPresentationPayload: WebhookPresentationProofDto,
         @Res() res: Response
     ): Promise<Response> {
+        proofPresentationPayload.type = 'Verification';
         this.logger.debug(`proofPresentationPayload ::: ${JSON.stringify(proofPresentationPayload)}`);
         const  webhookUrl = await this.verificationService._getWebhookUrl(proofPresentationPayload.contextCorrelationId);
     if (webhookUrl) {

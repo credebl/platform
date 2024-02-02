98eae913251916aae26ae55588d3a31e8e1799e3
bhavanakarwade <bhavana.karwade@ayanworks.com>
2024-02-02 17:50:28 +0530

    fix: remove unnecessary migration files
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- libs/prisma-service/prisma/migrations/20240202091638_connections_delete_image_url/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `imageUrl` on the `connections` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "connections" DROP COLUMN "imageUrl";

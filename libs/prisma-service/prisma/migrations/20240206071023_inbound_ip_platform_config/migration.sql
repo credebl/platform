3c7086c42c035b578d7ef6400a6a68da9b69583a
KulkarniShashank <shashank.kulkarni@ayanworks.com>
2024-02-06 12:42:52 +0530

    Added the migration for the inbound Ip
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- libs/prisma-service/prisma/migrations/20240206071023_inbound_ip_platform_config/migration.sql
+++ libs/prisma-service/prisma/migrations/20240206071023_inbound_ip_platform_config/migration.sql
@@ -2,9 +2,8 @@
   Warnings:
 
   - You are about to drop the column `lastInternalId` on the `platform_config` table. All the data in the column will be lost.
-  - Added the required column `inboundEndpoint` to the `platform_config` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
 ALTER TABLE "platform_config" DROP COLUMN "lastInternalId",
-ADD COLUMN     "inboundEndpoint" VARCHAR NOT NULL;
+ADD COLUMN     "inboundEndpoint" VARCHAR;

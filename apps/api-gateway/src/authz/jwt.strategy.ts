53051efb7fe991a13bebf4f8063a6d39e7dabc77
sanjay-k1910 <sanjay.khatal@ayanworks.com>
2024-02-09 15:50:22 +0530

    fix: sonarlint issue
    Signed-off-by: sanjay-k1910 <sanjay.khatal@ayanworks.com>

--- apps/api-gateway/src/authz/jwt.strategy.ts
+++ apps/api-gateway/src/authz/jwt.strategy.ts
@@ -1,10 +1,9 @@
 import * as dotenv from 'dotenv';
 
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
+import { Injectable, Logger, UnauthorizedException, NotFoundException } from '@nestjs/common';
 
 import { JwtPayload } from './jwt-payload.interface';
-import { NotFoundException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { UserService } from '../user/user.service';
 import * as jwt from 'jsonwebtoken';

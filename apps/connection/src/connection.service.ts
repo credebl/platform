84aea3289f9d3ff745d8e091b6b40bda1652439e
bhavanakarwade <bhavana.karwade@ayanworks.com>
2024-02-13 15:21:26 +0530

    fix: dco error
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>

--- apps/connection/src/connection.service.ts
+++ apps/connection/src/connection.service.ts
@@ -56,12 +56,13 @@ export class ConnectionService {
       }
 
       this.logger.log(`logoUrl:::, ${organisation.logoUrl}`);
-      
+
       const connectionPayload = {
         multiUseInvitation: multiUseInvitation || true,
         autoAcceptConnection: autoAcceptConnection || true,
         alias: alias || undefined,
-        imageUrl: organisation.logoUrl ? organisation.logoUrl : imageUrl ? imageUrl : undefined,
+        // imageUrl: organisation.logoUrl ? organisation.logoUrl : imageUrl ? imageUrl : undefined,
+        imageUrl: organisation?.logoUrl || imageUrl || undefined,
         label: organisation.name,
         goal: goal || undefined,
         goalCode: goalCode || undefined,

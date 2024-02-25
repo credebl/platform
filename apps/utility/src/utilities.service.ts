aedd6a1ca5e8ee2a22fb8e45a2155c36887af73a
Krishna <krishna.waske@ayanworks.com>
2024-02-25 17:47:21 +0530

    Utilility endpoint for storing endpoint and returning URL
    Signed-off-by: Krishna <krishna.waske@ayanworks.com>
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/utility/src/utilities.service.ts
+++ apps/utility/src/utilities.service.ts
@@ -4,6 +4,7 @@ import { UtilitiesRepository } from './utilities.repository';
 import { AwsService } from '@credebl/aws';
 // import { IUtilities } from '../interfaces/shortening-url.interface';
 import { S3 } from 'aws-sdk';
+import { IStoreObject } from '../interfaces/shortening-url.interface';
 
 @Injectable()
 export class UtilitiesService {
@@ -49,12 +50,15 @@ export class UtilitiesService {
         }
     }
 
-    async storeObject(payload: {persistent: boolean, storeObj: object}): Promise<string> {
+    async storeObject(payload: {persistent: boolean, storeObj: IStoreObject}): Promise<string> {
         try {
+            // eslint-disable-next-line no-console
+            console.log('received here. StoreObj::::::', JSON.stringify(payload.storeObj.data));
             const timestamp = Date.now();
-            const uploadResult:S3.ManagedUpload.SendData = await this.awsService.storeObject(payload.persistent, timestamp, payload.storeObj);
+            const uploadResult:S3.ManagedUpload.SendData = await this.awsService.storeObject(payload.persistent, timestamp, payload.storeObj.data);
             const url: string = `https://${uploadResult.Bucket}.s3.${process.env.AWS_S3_STOREOBJECT_REGION}.amazonaws.com/${uploadResult.Key}`;
             return url;
+            // return 'success';
         } catch (error) {
             throw new Error('An error occurred while uploading data to S3.');
         }

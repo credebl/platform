c070552016010fa9613f844a3d5e1c7f580853cd
pranalidhanavade <pranali.dhanavade@ayanworks.com>
2024-02-26 19:04:40 +0530

    fix: extra attribute in issuance payload
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/issuance/src/issuance.service.ts
+++ apps/issuance/src/issuance.service.ts
@@ -114,7 +114,7 @@ export class IssuanceService {
             // eslint-disable-next-line @typescript-eslint/no-unused-vars
             attributes: (attributes).map(({ isRequired, ...rest }) => rest),
             credentialDefinitionId
-
+            
           }
         },
         autoAcceptCredential: autoAcceptCredential || 'always',
@@ -525,7 +525,6 @@ const credefError = [];
                     `credentialOffer.${index}.attributes.${i}.Attribute ${schemaAttribute.attributeName} is required`
                   );
                 }
-                //
                 
               });
             });
@@ -564,45 +563,22 @@ const credefError = [];
       const sendEmailForCredentialOffer = async (iterator, emailId, index): Promise<boolean> => {
         const iterationNo = index + 1;
         try {
-          if (IssueCredentialType.INDY === credentialType) {
-          
-            outOfBandIssuancePayload = {
-              protocolVersion: protocolVersion || 'v1',
-              credentialFormats: {
-                indy: {
-                  attributes: iterator.attributes || attributes,
-                  credentialDefinitionId
-                }
-              },
-              autoAcceptCredential: outOfBandCredential.autoAcceptCredential || 'always',
-              comment,
-              goalCode: outOfBandCredential.goalCode || undefined,
-              parentThreadId: outOfBandCredential.parentThreadId || undefined,
-              willConfirm: outOfBandCredential.willConfirm || undefined,
-              label: outOfBandCredential.label || undefined,
-              imageUrl: organisation?.logoUrl || outOfBandCredential?.imageUrl
-            };
-          }
-
-          if (IssueCredentialType.JSONLD === credentialType) {
-            outOfBandIssuancePayload = {
-              protocolVersion:'v2',
-              credentialFormats: {
-                jsonld: {
-                  credential: iterator.credential,
-                  options: iterator.options
-                }
-              },
-              autoAcceptCredential: outOfBandCredential.autoAcceptCredential || 'always',
-              comment,
-              goalCode: outOfBandCredential.goalCode || undefined,
-              parentThreadId: outOfBandCredential.parentThreadId || undefined,
-              willConfirm: outOfBandCredential.willConfirm || undefined,
-              label: outOfBandCredential.label || undefined,
-              imageUrl: organisation?.logoUrl || outOfBandCredential?.imageUrl
-            };
-          }
-          
+          const outOfBandIssuancePayload = {
+            protocolVersion: protocolVersion || 'v1',
+            credentialFormats: {
+              indy: {
+                // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                attributes: (iterator.attributes || attributes).map(({ isRequired, ...rest }) => rest),
+                credentialDefinitionId
+              }
+            },
+            autoAcceptCredential: outOfBandCredential.autoAcceptCredential || 'always',
+            comment,
+            goalCode: outOfBandCredential.goalCode || undefined,
+            parentThreadId: outOfBandCredential.parentThreadId || undefined,
+            willConfirm: outOfBandCredential.willConfirm || undefined,
+            label: outOfBandCredential.label || undefined
+          };
 
           this.logger.log(`outOfBandIssuancePayload ::: ${JSON.stringify(outOfBandIssuancePayload)}`);
 

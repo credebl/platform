73bff37249ab3113429f2f8c6bde8a03dd734466
Nishad <nishad.shirsat@ayanworks.com>
2024-02-27 18:55:14 +0530

    fixed bulk issuance upload issue
    Signed-off-by: Nishad <nishad.shirsat@ayanworks.com>
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/issuance/src/issuance.service.ts
+++ apps/issuance/src/issuance.service.ts
@@ -711,6 +711,8 @@ const credefError = [];
     try {
       const schemaResponse: SchemaDetails = await this.issuanceRepository.getCredentialDefinitionDetails(credentialDefinitionId);
 
+      // console.log(`Export CSV`);
+      
       const jsonData = [];
       const attributesArray = JSON.parse(schemaResponse.attributes);
 
@@ -822,11 +824,8 @@ const credefError = [];
 
     } catch (error) {
       this.logger.error(`error in validating credentials : ${error.response}`);
-      throw  new Error(error.response.message ? error.response.message : error);
-    } finally {
-      // await this.awsService.deleteFile(importFileDetails.fileKey);
-      // this.logger.error(`Deleted uploaded file after processing.`);
-    }
+      throw  new RpcException(error.response ? error.response : error);
+    } 
   }
 
   async previewFileDataForIssuance(

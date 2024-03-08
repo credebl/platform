5e2c69132949690b8de5a7cd2d45bb5403335278
Krishna <krishna.waske@ayanworks.com>
2024-03-08 16:38:47 +0530

    fix: remove unnecessary logs
    Signed-off-by: Krishna <krishna.waske@ayanworks.com>
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/issuance/src/issuance.service.ts
+++ apps/issuance/src/issuance.service.ts
@@ -669,8 +669,6 @@ async sendEmailForCredentialOffer(sendEmailCredentialOffer: SendEmailCredentialO
       };
     }
 
-    this.logger.log(`outOfBandIssuancePayload ::: ${JSON.stringify(outOfBandIssuancePayload)}`);
-
     const credentialCreateOfferDetails = await this._outOfBandCredentialOffer(outOfBandIssuancePayload, url, apiKey);
 
     if (!credentialCreateOfferDetails) {

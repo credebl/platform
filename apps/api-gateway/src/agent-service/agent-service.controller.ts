ea97948584c48bab180b115ef4e4cf70da5419e5
bhavanakarwade <bhavana.karwade@ayanworks.com>
2024-03-11 21:14:13 +0530

    feat: support multiple did methods
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>

--- apps/api-gateway/src/agent-service/agent-service.controller.ts
+++ apps/api-gateway/src/agent-service/agent-service.controller.ts
@@ -32,6 +32,9 @@ import { CustomExceptionFilter } from 'apps/api-gateway/common/exception-handler
 import { Roles } from '../authz/decorators/roles.decorator';
 import { OrgRoles } from 'libs/org-roles/enums';
 import { OrgRolesGuard } from '../authz/guards/org-roles.guard';
+import { CreateDidDto } from './dto/create-did.dto';
+import { validateDid } from '@credebl/common/did.validator';
+import { CreateWalletDto } from './dto/create-wallet.dto';
 
 const seedLength = 32;
 @UseFilters(CustomExceptionFilter)
@@ -69,6 +72,29 @@ export class AgentController {
       statusCode: HttpStatus.OK,
       message: ResponseMessages.agent.success.health,
       data: agentData
+    };
+
+    return res.status(HttpStatus.OK).json(finalResponse);
+
+  }
+
+  @Get('/orgs/agents/ledgerConfig')
+  @ApiOperation({
+    summary: 'Get the ledger config details',
+    description: 'Get the ledger config details'
+  })
+  @UseGuards(AuthGuard('jwt'))
+  async getLedgerDetails(
+    @User() reqUser: user,
+    @Res() res: Response
+  ): Promise<Response> {
+
+    const ledgerConfigData = await this.agentService.getLedgerConfig(reqUser);
+
+    const finalResponse: IResponse = {
+      statusCode: HttpStatus.OK,
+      message: ResponseMessages.agent.success.ledgerConfig,
+      data: ledgerConfigData
     };
 
     return res.status(HttpStatus.OK).json(finalResponse);
@@ -154,13 +180,13 @@ export class AgentController {
 
     createTenantDto.orgId = orgId;
 
-    if (seedLength !== createTenantDto.seed.length) {
-      this.logger.error(`seed must be at most 32 characters`);
-      throw new BadRequestException(
-        ResponseMessages.agent.error.seedCharCount,
-        { cause: new Error(), description: ResponseMessages.errorMessages.badRequest }
-      );
-    }
+    // if (seedLength !== createTenantDto.seed.length) {
+    //   this.logger.error(`seed must be at most 32 characters`);
+    //   throw new BadRequestException(
+    //     ResponseMessages.agent.error.seedCharCount,
+    //     { cause: new Error(), description: ResponseMessages.errorMessages.badRequest }
+    //   );
+    // }
 
     const tenantDetails = await this.agentService.createTenant(createTenantDto, user);
 
@@ -172,4 +198,95 @@ export class AgentController {
 
     return res.status(HttpStatus.CREATED).json(finalResponse);
   }
+
+  /**
+   * Create wallet
+   * @param orgId 
+   * @returns wallet
+   */
+     @Post('/orgs/:orgId/agents/createWallet')
+     @ApiOperation({
+       summary: 'Create wallet',
+       description: 'Create wallet'
+     })
+     @UseGuards(AuthGuard('jwt'), OrgRolesGuard)
+     @Roles(OrgRoles.OWNER, OrgRoles.ADMIN)
+     @ApiResponse({ status: HttpStatus.CREATED, description: 'Success', type: ApiResponseDto })
+     async createWallet(
+       @Param('orgId') orgId: string,
+       @Body() createWalletDto: CreateWalletDto,
+       @User() user: user,
+       @Res() res: Response
+     ): Promise<Response> {
+   
+      createWalletDto.orgId = orgId;
+      const walletDetails = await this.agentService.createWallet(createWalletDto, user);
+   
+       const finalResponse: IResponse = {
+         statusCode: HttpStatus.CREATED,
+         message: ResponseMessages.agent.success.createWallet,
+         data: walletDetails
+       };
+   
+       return res.status(HttpStatus.CREATED).json(finalResponse);
+     }
+  
+  // This function will be used after multiple did method implementation in create wallet
+   /**
+   * Create did
+   * @param orgId 
+   * @returns did
+   */
+   @Post('/orgs/:orgId/agents/createDid')
+   @ApiOperation({
+     summary: 'Create did',
+     description: 'Create did'
+   })
+   @UseGuards(AuthGuard('jwt'), OrgRolesGuard)
+   @Roles(OrgRoles.OWNER, OrgRoles.ADMIN)
+   @ApiResponse({ status: HttpStatus.CREATED, description: 'Success', type: ApiResponseDto })
+   async createDid(
+     @Param('orgId') orgId: string,
+     @Body() createDidDto: CreateDidDto,
+     @User() user: user,
+     @Res() res: Response
+   ): Promise<Response> {
+  
+    validateDid(createDidDto);
+
+     const didDetails = await this.agentService.createDid(createDidDto, orgId, user);
+ 
+     const finalResponse: IResponse = {
+       statusCode: HttpStatus.CREATED,
+       message: ResponseMessages.agent.success.createDid,
+       data: didDetails
+     };
+ 
+     return res.status(HttpStatus.CREATED).json(finalResponse);
+   }
+
+    /**
+   * Create Secp256k1 key pair for polygon DID
+   * @param orgId 
+   * @returns Secp256k1 key pair for polygon DID
+   */
+    @Post('/orgs/:orgId/agents/polygon/create-keys')
+    @UseGuards(AuthGuard('jwt'), OrgRolesGuard)
+    @Roles(OrgRoles.OWNER, OrgRoles.ADMIN, OrgRoles.PLATFORM_ADMIN, OrgRoles.ISSUER, OrgRoles.VERIFIER)
+    @ApiResponse({ status: HttpStatus.CREATED, description: 'Success', type: ApiResponseDto })
+    async createSecp256k1KeyPair(
+      @Param('orgId') orgId: string,
+      @Res() res: Response
+    ): Promise<Response> {
+   
+      const didDetails = await this.agentService.createSecp256k1KeyPair(orgId);
+  
+      const finalResponse: IResponse = {
+        statusCode: HttpStatus.CREATED,
+        message: ResponseMessages.agent.success.createKeys,
+        data: didDetails
+      };
+  
+      return res.status(HttpStatus.CREATED).json(finalResponse);
+    }
 }

8b56555afdd340c73b0d2c7fd591cf34ec02a551
tipusinghaw <tipu.singh@ayanworks.com>
2024-03-19 13:03:14 +0530

    fix: multiple error messages
    Signed-off-by: tipusinghaw <tipu.singh@ayanworks.com>
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/ledger/src/schema/schema.service.ts
+++ apps/ledger/src/schema/schema.service.ts
@@ -277,7 +277,6 @@ export class SchemaService extends BaseService {
       this.logger.error(
         `[createSchema] - outer Error: ${JSON.stringify(error)}`
       );
-      //need to discuss the multiple error message
       throw new RpcException(error.error ? error.error.message : error.message);
     }
   }
@@ -325,12 +324,7 @@ export class SchemaService extends BaseService {
         ).toPromise()
         .catch(error => {
           this.logger.error(`Error in creating WC3 schema : ${JSON.stringify(error)}`);
-          throw new HttpException(
-            {
-              status: error.statusCode,  
-              error: error.error,
-              message: error.message
-            }, error.error);
+          throw new RpcException(error.error ? error.error.message : error.message);
         });
       return W3CSchemaResponse;  
   }

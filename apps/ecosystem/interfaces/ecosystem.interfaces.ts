9f4b77fe0c5798055c3fe504b5582a36aff71d9c
bhavanakarwade <bhavana.karwade@ayanworks.com>
2024-03-20 20:10:22 +0530

    fix: resolved sonarlint issues
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/ecosystem/interfaces/ecosystem.interfaces.ts
+++ apps/ecosystem/interfaces/ecosystem.interfaces.ts
@@ -220,15 +220,6 @@ export interface LedgerDetails {
   networkUrl: string;
 }
 
-interface EcosystemRole {
-  id: string;
-  name: string;
-  description: string;
-  createDateTime: Date;
-  lastChangedDateTime: Date;
-  deletedAt: Date;
-}
-
 export interface EcosystemInvitationDetails {
   name: string;
   id: string;
@@ -377,37 +368,4 @@ export interface IEcosystemList {
   pageNumber: number;
   pageSize: number;
   search: string;
-}
-
-interface Ecosystem {
-  id: string;
-  name: string;
-  description: string;
-  logoUrl: string | null;
-  createDateTime: string;
-  lastChangedDateTime: string;
-  createdBy: string;
-  autoEndorsement: boolean;
-  ecosystemOrgs: EcosystemOrg[];
-}
-
-interface EcosystemOrg {
-  id: string;
-  orgId: string;
-  status: string;
-  createDateTime: string;
-  lastChangedDateTime: string;
-  ecosystemId: string;
-  ecosystemRoleId: string;
-  ecosystemRole: EcosystemRole;
-}
-
-export interface IPagination {
-  totalItems: number;
-  hasNextPage: boolean;
-  hasPreviousPage: boolean;
-  nextPage: number;
-  previousPage: number;
-  lastPage: number;
-  ecosystemList: Ecosystem[];
 }

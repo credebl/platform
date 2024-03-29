c6a4ad958a4e56fd357f5b8d12514a210c1399c4
Shashank Kulkarni <44693969+KulkarniShashank@users.noreply.github.com>
2024-03-29 20:41:35 +0530

    feat: out-of-band connection invitation (#623)
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/api-gateway/src/connection/enums/connections.enum.ts
+++ apps/api-gateway/src/connection/enums/connections.enum.ts
@@ -10,3 +10,8 @@ export enum Connections {
     complete = 'complete',
     abandoned = 'abandoned'
 }
+
+export declare enum HandshakeProtocol {
+    Connections = "https://didcomm.org/connections/1.0",
+    DidExchange = "https://didcomm.org/didexchange/1.0"
+}

3f7f6d6d15bc6bda4e9dce743c6c949ff7dd40cf
pranalidhanavade <pranali.dhanavade@ayanworks.com>
2024-04-11 20:51:19 +0530

    fix: security vulnerabilities
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>

--- apps/api-gateway/src/connection/dtos/get-all-connections.dto.ts
+++ apps/api-gateway/src/connection/dtos/get-all-connections.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Transform, Type } from 'class-transformer';
-import { IsEnum, IsOptional } from 'class-validator';
+import { IsEnum, IsOptional, Max, Min } from 'class-validator';
 import { SortValue } from '../../enum';
 import { trim } from '@credebl/common/cast.helper';
 import { SortFields } from 'apps/connection/src/enum/connection.enum';
@@ -12,6 +12,8 @@ export class GetAllConnectionsDto {
 
   @ApiProperty({ required: false, example: '10' })
   @IsOptional()
+  @Min(1, { message: 'Page size must be greater than 0' })
+  @Max(100, { message: 'Page size must be less than 100' })
   pageSize: number = 10;
 
   @ApiProperty({ required: false })

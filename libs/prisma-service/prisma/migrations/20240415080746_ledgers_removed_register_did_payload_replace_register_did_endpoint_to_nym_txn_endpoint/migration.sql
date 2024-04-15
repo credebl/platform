182f224fd5e699e51dde14cfecd8fd45d44173d3
bhavanakarwade <bhavana.karwade@ayanworks.com>
2024-04-15 13:48:15 +0530

    refactor: ledger table
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>

--- libs/prisma-service/prisma/migrations/20240415080746_ledgers_removed_register_did_payload_replace_register_did_endpoint_to_nym_txn_endpoint/migration.sql
+++ libs/prisma-service/prisma/migrations/20240415080746_ledgers_removed_register_did_payload_replace_register_did_endpoint_to_nym_txn_endpoint/migration.sql
@@ -3,10 +3,9 @@
 
   - You are about to drop the column `registerDIDEndpoint` on the `ledgers` table. All the data in the column will be lost.
   - You are about to drop the column `registerDIDPayload` on the `ledgers` table. All the data in the column will be lost.
+  - Added the required column `nymTxnEndpoint` to the `ledgers` table without a default value. This is not possible if the table is not empty.
 
 */
 -- AlterTable
-ALTER TABLE "ledgers" DROP COLUMN "registerDIDEndpoint",
-DROP COLUMN "registerDIDPayload",
-ADD COLUMN     "registeredDIDEndpoint" VARCHAR,
-ADD COLUMN     "registeredDIDPayload" JSONB;
+ALTER TABLE "ledgers" DROP COLUMN "registerDIDPayload";
+ALTER TABLE "ledgers" RENAME COLUMN "registerDIDEndpoint" TO "nymTxnEndpoint";

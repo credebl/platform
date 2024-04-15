36cc698cfd99a12f57fe19557df4c76e61f7695a
bhavanakarwade <bhavana.karwade@ayanworks.com>
2024-04-15 14:39:01 +0530

    refactor: update import of enum
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>

---
+++ apps/api-gateway/src/schema/enum/schema.enum.ts
@@ -0,0 +1,9 @@
+export enum SortFields {
+    ID = 'id',
+    CREATED_DATE_TIME = 'createDateTime',
+    NAME = 'name',
+    VERSION = 'version',
+    LEDGER_ID = 'schemaLedgerId',
+    PUBLISHER_DID = 'publisherDid',
+    ISSUER_ID = 'issuerId'
+}

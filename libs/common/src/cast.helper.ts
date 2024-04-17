4fe396b9de344659274ade5e9b6b0cef3525dfab
Krishna <krishna.waske@ayanworks.com>
2024-04-17 13:30:47 +0530

    fix: logo uri validation in image update for org for security testing
    Signed-off-by: Krishna <krishna.waske@ayanworks.com>
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- libs/common/src/cast.helper.ts
+++ libs/common/src/cast.helper.ts
@@ -1,4 +1,5 @@
-import { ValidationArguments, ValidationOptions, registerDecorator } from "class-validator";
+import { BadRequestException } from '@nestjs/common';
+import { ValidationArguments, ValidationOptions, ValidatorConstraint, ValidatorConstraintInterface, isBase64, isMimeType, registerDecorator } from 'class-validator';
 
 interface ToNumberOptions {
     default?: number;
@@ -90,3 +91,38 @@ export function isSafeString(value: string): boolean {
         }
       });
     };
+
+@ValidatorConstraint({ name: 'customText', async: false })
+export class ImageBase64Validator implements ValidatorConstraintInterface {
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/explicit-function-return-type, @typescript-eslint/no-unused-vars
+  validate(value: string, args: ValidationArguments) {
+    // Implement your custom validation logic here
+    // return text === 'hello';
+    if (!value || 'string' !== typeof value) {
+      throw new BadRequestException('Invalid base64 string');
+    }
+    const parts = value.split(',');
+    if (2 !== parts.length) {
+      throw new BadRequestException('Invalid data URI');
+    }
+    // eslint-disable-next-line prefer-destructuring
+    const mimeType = parts[0].split(';')[0].split(':')[1];
+    // eslint-disable-next-line prefer-destructuring
+    const base64Data = parts[1];
+
+    // Validate MIME type
+    if (!isMimeType(mimeType)) {
+      throw new BadRequestException('Please provide valid MIME type');
+    }
+    // Validate base64 data
+    if (!isBase64(base64Data) || '' == base64Data || null == base64Data) {
+      throw new BadRequestException('Invalid base64 string');
+    }
+    return true;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/explicit-function-return-type, @typescript-eslint/no-unused-vars
+  defaultMessage(_args: ValidationArguments) {
+    return 'Default message received from [ImageBase64Validator]';
+  }
+}

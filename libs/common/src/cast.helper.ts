16b846db0323e67746cfe03e9764958345a340c0
KulkarniShashank <shashank.kulkarni@ayanworks.com>
2024-04-18 13:42:29 +0530

    fix: removed the is not UUID validation in org name
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- libs/common/src/cast.helper.ts
+++ libs/common/src/cast.helper.ts
@@ -1,5 +1,5 @@
 import { BadRequestException } from '@nestjs/common';
-import { ValidationArguments, ValidationOptions, ValidatorConstraint, ValidatorConstraintInterface, isBase64, isMimeType, isUUID, registerDecorator } from 'class-validator';
+import { ValidationArguments, ValidationOptions, ValidatorConstraint, ValidatorConstraintInterface, isBase64, isMimeType, registerDecorator } from 'class-validator';
 
 interface ToNumberOptions {
     default?: number;
@@ -130,16 +130,17 @@ export class ImageBase64Validator implements ValidatorConstraintInterface {
   }
 }
 
-export const IsNotUUID = (validationOptions?: ValidationOptions): PropertyDecorator => (object: object, propertyName: string) => {
-  registerDecorator({
-    name: 'isNotUUID',
-    target: object.constructor,
-    propertyName,
-    options: validationOptions,
-    validator: {
-      validate(value) {
-        return !isUUID(value);
-      }
-    }
-  });
-};
+// IS NOT UUID validation 
+// export const IsNotUUID = (validationOptions?: ValidationOptions): PropertyDecorator => (object: object, propertyName: string) => {
+//   registerDecorator({
+//     name: 'isNotUUID',
+//     target: object.constructor,
+//     propertyName,
+//     options: validationOptions,
+//     validator: {
+//       validate(value) {
+//         return !isUUID(value);
+//       }
+//     }
+//   });
+// };

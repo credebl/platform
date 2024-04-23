6950bef9a43a4976a117b0d7bff17e98296fa0d9
tipusinghaw <126460794+tipusinghaw@users.noreply.github.com>
2024-04-23 12:46:51 +0530

    feat: multi-did creation implementation for an organization (#667)
    * feat: multi did support
    
    Signed-off-by: tipusinghaw <tipu.singh@ayanworks.com>
    
    * refactor: added validation
    
    Signed-off-by: tipusinghaw <tipu.singh@ayanworks.com>
    
    * refactor: added logic for pervious primary DID
    
    Signed-off-by: tipusinghaw <tipu.singh@ayanworks.com>
    
    * refactor: changed primaryDid and added size to did veriable
    
    Signed-off-by: tipusinghaw <tipu.singh@ayanworks.com>
    
    * fix: resloved commits
    
    Signed-off-by: tipusinghaw <tipu.singh@ayanworks.com>
    
    * fix: updated the import url
    
    Signed-off-by: tipusinghaw <tipu.singh@ayanworks.com>
    
    ---------
    
    Signed-off-by: tipusinghaw <tipu.singh@ayanworks.com>

--- apps/api-gateway/src/organization/organization.controller.ts
+++ apps/api-gateway/src/organization/organization.controller.ts
@@ -25,6 +25,7 @@ import { ClientCredentialsDto } from './dtos/client-credentials.dto';
 import { PaginationDto } from '@credebl/common/dtos/pagination.dto';
 import { validate as isValidUUID } from 'uuid';
 import { UserAccessGuard } from '../authz/guards/user-access-guard';
+import { PrimaryDid } from './dtos/set-primary-did.dto';
 
 @UseFilters(CustomExceptionFilter)
 @Controller('orgs')
@@ -318,6 +319,29 @@ export class OrganizationController {
 
     return res.status(HttpStatus.OK).json(finalResponse);
   }
+
+   /**
+  * @returns DID list of organization
+  */
+
+   @Get('/:orgId/dids')
+   @Roles(OrgRoles.OWNER, OrgRoles.ADMIN, OrgRoles.ISSUER)
+   @ApiBearerAuth()
+   @UseGuards(AuthGuard('jwt'), OrgRolesGuard)
+   @ApiResponse({ status: HttpStatus.OK, description: 'Success', type: ApiResponseDto })
+   @ApiOperation({ summary: 'Fetch organization DIDs', description: 'Get all DIDs from organization' })
+  
+   async getAllDidByOrgId(@Param('orgId') orgId: string, @Res() res: Response): Promise<Response> {
+     const users = await this.organizationService.getDidList(orgId);
+     const finalResponse: IResponse = {
+       statusCode: HttpStatus.OK,
+       message: ResponseMessages.organisation.success.orgDids,
+       data: users
+     };
+ 
+     return res.status(HttpStatus.OK).json(finalResponse);
+   }
+
 /**
  * @returns organization details
  */
@@ -341,6 +365,26 @@ export class OrganizationController {
     return res.status(HttpStatus.CREATED).json(finalResponse);
   }
 
+
+  /**
+ * @returns success message
+ */
+
+  @Post('/:orgId/primary-did')
+  @Roles(OrgRoles.OWNER, OrgRoles.ADMIN, OrgRoles.ISSUER, OrgRoles.VERIFIER, OrgRoles.MEMBER)
+  @ApiOperation({ summary: 'Set primary DID', description: 'Set primary DID for an organization' })
+  @ApiResponse({ status: HttpStatus.CREATED, description: 'Success', type: ApiResponseDto })
+  @UseGuards(AuthGuard('jwt'), OrgRolesGuard)
+  @ApiBearerAuth()
+  async setPrimaryDid(@Param('orgId') orgId: string, @Body() primaryDidPayload: PrimaryDid, @Res() res: Response): Promise<Response> {
+    const orgData = await this.organizationService.setPrimaryDid(primaryDidPayload, orgId);
+    const finalResponse: IResponse = {
+      statusCode: HttpStatus.CREATED,
+      message: ResponseMessages.organisation.success.primaryDid,
+      data: orgData
+    };
+    return res.status(HttpStatus.CREATED).json(finalResponse);
+  }
   /**
    * 
    * @param orgId 

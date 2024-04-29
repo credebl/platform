4506745242f4413ba423fa0193960c1984c5efaf
bhavanakarwade <bhavana.karwade@ayanworks.com>
2024-04-29 14:28:07 +0530

    refactor: dto example
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>

--- apps/api-gateway/src/issuance/dtos/multi-connection.dto.ts
+++ apps/api-gateway/src/issuance/dtos/multi-connection.dto.ts
@@ -48,15 +48,42 @@ export class IssueCredentialDto extends CredentialsIssuanceDto {
     @ApiProperty({
       example: [
           {
-              connectionId: 'string',
-              attributes: [
+              'connectionId': 'string',
+              'attributes': [
                   {
-                      value: 'string',
-                      name: 'string'
+                      'value': 'string',
+                      'name': 'string'
                   }
-              ]
+              ],
+              'credential': {
+                '@context': [
+                  'https://www.w3.org/2018/credentials/v1',
+                  'https://www.w3.org/2018/credentials/examples/v1'
+                ],
+                'type': [
+                  'VerifiableCredential',
+                  'UniversityDegreeCredential'
+                ],
+                'issuer': {
+                  'id': 'did:key:z6Mkn72LVp3mq1fWSefkSMh5V7qrmGfCV4KH3K6SoTM21ouM'
+                },
+                'issuanceDate': '2019-10-12T07:20:50.52Z',
+                'credentialSubject': {
+                  'id': 'did:key:z6Mkn72LVp3mq1fWSefkSMh5V7qrmGfCV4KH3K6SoTM21ouM',
+                  'degree': {
+                    'type': 'BachelorDegree',
+                    'name': 'Bachelor of Science and Arts'
+                  }
+                }
+              },
+              'options': {
+                'proofType': 'Ed25519Signature2018',
+                'proofPurpose': 'assertionMethod'
+              }
+      
           }
       ]
+      
     })
     @IsArray()
     @ValidateNested({ each: true })

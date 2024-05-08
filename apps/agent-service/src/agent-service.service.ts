4b2f2304b3e363c1f81bccda98fd0a683a1695bc
tipusinghaw <126460794+tipusinghaw@users.noreply.github.com>
2024-05-08 14:16:54 +0530

    fix: store did document while creating did (#706)
    * fix: set primary did issue
    
    Signed-off-by: tipusinghaw <tipu.singh@ayanworks.com>
    
    * fix: did documentation issue
    
    Signed-off-by: tipusinghaw <tipu.singh@ayanworks.com>
    
    * changes error message in get did details
    
    Signed-off-by: tipusinghaw <tipu.singh@ayanworks.com>
    
    ---------
    
    Signed-off-by: tipusinghaw <tipu.singh@ayanworks.com>
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/agent-service/src/agent-service.service.ts
+++ apps/agent-service/src/agent-service.service.ts
@@ -868,12 +868,10 @@ export class AgentServiceService {
       }
 
       delete createDidPayload.isPrimaryDid;
+      
+      const didDetails = await this.commonService.httpPost(url, createDidPayload, { headers: { authorization: getApiKey } });
 
-      const didDetails = await this.commonService.httpPost(url, createDidPayload, {
-        headers: { authorization: getApiKey }
-      });
-
-      if (!didDetails) {
+      if (!didDetails || Object.keys(didDetails).length === 0) {
         throw new InternalServerErrorException(ResponseMessages.agent.error.createDid, {
           cause: new Error(),
           description: ResponseMessages.errorMessages.serverError
@@ -882,7 +880,7 @@ export class AgentServiceService {
       const createdDidDetails = {
         orgId,
         did: didDetails.did,
-        didDocument: didDetails.didDocument,
+        didDocument: didDetails.didDocument || didDetails.didDoc,
         isPrimaryDid,
         orgAgentId: agentDetails.id,
         userId: user.id

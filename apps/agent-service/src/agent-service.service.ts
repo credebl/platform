2d3f391305731d54e0019d8fbb420cb0fa77f4aa
KulkarniShashank <shashank.kulkarni@ayanworks.com>
2024-05-16 13:34:06 +0530

    fix: added validation for wallet name
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/agent-service/src/agent-service.service.ts
+++ apps/agent-service/src/agent-service.service.ts
@@ -947,9 +947,7 @@ export class AgentServiceService {
       }
       
       delete createDidPayload.isPrimaryDid;
-      
-      delete createDidPayload.isPrimaryDid;
-      
+            
       const didDetails = await this.commonService.httpPost(url, createDidPayload, { headers: { authorization: getApiKey } });
 
       if (!didDetails || Object.keys(didDetails).length === 0) {

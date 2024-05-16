c9868b346dccab0097a3eef7ea8599aa43bdf9a9
KulkarniShashank <shashank.kulkarni@ayanworks.com>
2024-05-16 12:37:05 +0530

    fix: validation for inboundEndpoint domain
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/agent-provisioning/AFJ/scripts/start_agent_ecs.sh
+++ apps/agent-provisioning/AFJ/scripts/start_agent_ecs.sh
@@ -94,15 +94,17 @@ echo "AGENT SPIN-UP STARTED"
 # Define a regular expression pattern for IP address
 IP_REGEX="^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$"
 
-# Check if the input is a domain
-if echo "$INBOUND_ENDPOINT" | grep -qP "^[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"; then
+# Check if INBOUND_ENDPOINT is a domain or IP address
+if [[ $INBOUND_ENDPOINT =~ ^https?://[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$ ]]; then
   echo "INBOUND_ENDPOINT is a domain: $INBOUND_ENDPOINT"
-  AGENT_ENDPOINT=$INBOUND_ENDPOINT
+  # Extracting the domain name without the protocol
+  AGENT_ENDPOINT=$(echo "$INBOUND_ENDPOINT" | sed 's/^https\?:\/\///')
 else
   # Check if the input is an IP address
   if [[ $INBOUND_ENDPOINT =~ $IP_REGEX ]]; then
     echo "INBOUND_ENDPOINT is an IP address: $INBOUND_ENDPOINT"
-    AGENT_ENDPOINT="${PROTOCOL}://${EXTERNAL_IP}:${INBOUND_PORT}"
+    # Adding the protocol to the IP address
+    AGENT_ENDPOINT="${PROTOCOL}://${INBOUND_ENDPOINT}:${INBOUND_PORT}"
   else
     echo "Invalid input for INBOUND_ENDPOINT: $INBOUND_ENDPOINT"
   fi

b7551b22223996f1e3fdc3803a172402723d8b1e
Krishna <krishna.waske@ayanworks.com>
2024-05-16 19:35:02 +0530

    update: multi connection issuance DTO
    Signed-off-by: Krishna <krishna.waske@ayanworks.com>

--- apps/api-gateway/src/issuance/dtos/multi-connection.dto.ts
+++ apps/api-gateway/src/issuance/dtos/multi-connection.dto.ts
@@ -1,13 +1,10 @@
-import { ApiExtraModels, ApiProperty, ApiPropertyOptional, getSchemaPath } from '@nestjs/swagger';
-import { ArrayMinSize, IsArray, IsBoolean, IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { ApiExtraModels, ApiProperty, getSchemaPath } from '@nestjs/swagger';
+import { ArrayMaxSize, ArrayMinSize, IsNotEmpty, IsNotEmptyObject, IsString, ValidateNested } from 'class-validator';
 import { Transform } from 'class-transformer';
-import { AutoAccept } from '@credebl/enum/enum';
 import { trim } from '@credebl/common/cast.helper';
-import { AnonCredsDto, CredentialsIssuanceDto, IndyDto, JsonLdDto } from './issuance.dto';
+import { AnonCredsDto, IndyDto, IssuanceFields, JsonLdDto } from './issuance.dto';
 
-@ApiExtraModels(AnonCredsDto)
-@ApiExtraModels(JsonLdDto)
-@ApiExtraModels(IndyDto)
+@ApiExtraModels(AnonCredsDto, JsonLdDto, IndyDto)
 class ConnectionAttributes {
     @ApiProperty({ example: 'string' })
     @IsNotEmpty({ message: 'connectionId is required' })
@@ -15,114 +12,27 @@ class ConnectionAttributes {
     @Transform(({ value }) => trim(value))
     connectionId: string;
 
-    // Note: Anon creds default
-    // @ApiProperty({
-    //   example: [
-    //     {
-    //       value: 'string',
-    //       name: 'string'
-    //     }
-    //   ]
-    // })
-    // @IsArray()
-    // @ValidateNested({ each: true })
-    // @ArrayMinSize(1)
-    // @IsNotEmpty({ message: 'Please provide valid attributes' })
-    // @Type(() => Attribute)
-    // @IsOptional()
-    // attributes?: Attribute[];
-
-    // @ApiProperty()
-    // @IsNotEmpty({ message: 'Please provide valid credential' })
-    // @IsObject({ message: 'credential should be an object' })
-    // @Type(() => Credential)
-    // @IsOptional()
-    // @ValidateNested({ each: true })
-    // credential?: Credential;
-
     @ApiProperty({
-      anyOf: [
+      type: Object,
+      oneOf: [
         { $ref: getSchemaPath(AnonCredsDto) },
         { $ref: getSchemaPath(JsonLdDto) },
         { $ref: getSchemaPath(IndyDto) }
       ]
     })
+    @IsNotEmptyObject()
+    @ValidateNested({each: true})
     credentialFormats: AnonCredsDto | JsonLdDto | IndyDto;
-
-    // Note: Anon creds default
-    // @ApiProperty()
-    // @IsNotEmpty({ message: 'Please provide valid options' })
-    // @IsObject({ message: 'options should be an object' })
-    // @Type(() => JsonLdCredentialDetailOptions)
-    // @IsOptional()
-    // @ValidateNested({ each: true })
-    // options?:JsonLdCredentialDetailOptions;
 }
 
-export class IssueCredentialDto extends CredentialsIssuanceDto {
+export class IssueCredentialDto extends IssuanceFields {
     @ApiProperty({
       type: [ConnectionAttributes]
-      // example: [
-      //     {
-      //         'connectionId': 'string',
-      //         'attributes': [
-      //             {
-      //                 'value': 'string',
-      //                 'name': 'string'
-      //             }
-      //         ],
-      //         'credential': {
-      //           '@context': [
-      //             'https://www.w3.org/2018/credentials/v1',
-      //             'https://www.w3.org/2018/credentials/examples/v1'
-      //           ],
-      //           'type': [
-      //             'VerifiableCredential',
-      //             'UniversityDegreeCredential'
-      //           ],
-      //           'issuer': {
-      //             'id': 'did:key:z6Mkn72LVp3mq1fWSefkSMh5V7qrmGfCV4KH3K6SoTM21ouM'
-      //           },
-      //           'issuanceDate': '2019-10-12T07:20:50.52Z',
-      //           'credentialSubject': {
-      //             'id': 'did:key:z6Mkn72LVp3mq1fWSefkSMh5V7qrmGfCV4KH3K6SoTM21ouM',
-      //             'degree': {
-      //               'type': 'BachelorDegree',
-      //               'name': 'Bachelor of Science and Arts'
-      //             }
-      //           }
-      //         },
-      //         'options': {
-      //           'proofType': 'Ed25519Signature2018',
-      //           'proofPurpose': 'assertionMethod'
-      //         }
-      
-      //     }
-      // ]
     })
-    @IsArray()
+    // @IsArray()
     // @ValidateNested({ each: true })
     @ArrayMinSize(1)
-    // @ArrayMaxSize(Number(process.env.OOB_BATCH_SIZE), { message: `Limit reached (${process.env.OOB_BATCH_SIZE} connections max).` })
+    @ArrayMaxSize(Number(process.env.OOB_BATCH_SIZE), { message: `Limit reached (${process.env.OOB_BATCH_SIZE} connections max).` })
     @IsNotEmpty({ message: 'credentialData is required' })
-    // @Type(() => ConnectionAttributes)
     credentialData: ConnectionAttributes[];
-
-    @ApiPropertyOptional()
-    @IsOptional()
-    @IsString({ message: 'auto accept proof must be in string' })
-    @IsNotEmpty({ message: 'please provide valid auto accept proof' })
-    @IsEnum(AutoAccept, {
-        message: `Invalid auto accept credential. It should be one of: ${Object.values(AutoAccept).join(', ')}`
-    })
-    autoAcceptCredential?: string;
-
-    @ApiProperty({
-        example: false
-      })
-      @IsOptional()
-      @IsNotEmpty()
-      @IsBoolean({message: 'isShortenUrl must be boolean'})
-      isShortenUrl?: boolean;
-    
 }

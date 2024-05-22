d3c4874d8152bbc88e54f3ef1bf118ca33a43ebd
KulkarniShashank <shashank.kulkarni@ayanworks.com>
2024-05-22 19:19:33 +0530

    remove this type in the schema model
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/agent-service/src/agent-service.service.ts
+++ apps/agent-service/src/agent-service.service.ts
@@ -281,7 +281,6 @@ export class AgentServiceService {
         orgId,
         agentEndPoint: agentEndpoint,
         orgAgentTypeId,
-        ledgerId: null,
         apiKey: encryptedToken,
         userId
       });

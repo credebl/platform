94eaba4c98504b2288206bfd66d9fd5a22639f36
KulkarniShashank <shashank.kulkarni@ayanworks.com>
2024-05-22 19:19:33 +0530

    remove this type in the schema model
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- libs/common/src/cast.helper.ts
+++ libs/common/src/cast.helper.ts
@@ -183,13 +183,14 @@ export class AgentSpinupValidator {
   }
 }
 
+
 @ValidatorConstraint({ name: 'isHostPortOrDomain', async: false })
 export class IsHostPortOrDomainConstraint implements ValidatorConstraintInterface {
   validate(value: string): boolean {
-    // Regular expression for validating host:port or domain
+    // Regular expression for validating URL with host:port or domain
     const hostPortRegex =
-      /^(?:(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)):(?:\d{1,5})$/;
-    const domainRegex = /^(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}$/;
+      /^(http:\/\/|https:\/\/)?(?:(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)):(?:\d{1,5})$/;
+    const domainRegex = /^(http:\/\/|https:\/\/)?(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}$/;
 
     return hostPortRegex.test(value) || domainRegex.test(value);
   }

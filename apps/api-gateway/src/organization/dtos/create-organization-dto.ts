e79acfa7855b6c21bfab0c029e3496bcd0d919d3
pallavicoder <pallavi.ghule@ayanworks.com>
2024-06-10 19:34:36 +0530

    refactore:added metadata fields in org
    Signed-off-by: pallavicoder <pallavi.ghule@ayanworks.com>

--- apps/api-gateway/src/organization/dtos/create-organization-dto.ts
+++ apps/api-gateway/src/organization/dtos/create-organization-dto.ts
@@ -6,46 +6,64 @@ import { IsNotSQLInjection, trim } from '@credebl/common/cast.helper';
 
 @ApiExtraModels()
 export class CreateOrganizationDto {
+  @ApiProperty()
+  @Transform(({ value }) => trim(value))
+  @IsNotEmpty({ message: 'Organization name is required.' })
+  @MinLength(2, { message: 'Organization name must be at least 2 characters.' })
+  @MaxLength(50, { message: 'Organization name must be at most 50 characters.' })
+  @IsString({ message: 'Organization name must be in string format.' })
+  @IsNotSQLInjection({ message: 'Organization name is required.' })
+  name: string;
 
-    @ApiProperty()
-    @Transform(({ value }) => trim(value))
-    @IsNotEmpty({ message: 'Organization name is required.' })
-    @MinLength(2, { message: 'Organization name must be at least 2 characters.' })
-    @MaxLength(50, { message: 'Organization name must be at most 50 characters.' })
-    @IsString({ message: 'Organization name must be in string format.' })
-    @IsNotSQLInjection({ message: 'Organization name is required.' })
-    name: string;
+  @ApiPropertyOptional()
+  @Transform(({ value }) => trim(value))
+  @IsNotEmpty({ message: 'Description is required.' })
+  @MinLength(2, { message: 'Description must be at least 2 characters.' })
+  @MaxLength(255, { message: 'Description must be at most 255 characters.' })
+  @IsString({ message: 'Description must be in string format.' })
+  description: string;
 
-    @ApiPropertyOptional()
-    @Transform(({ value }) => trim(value))
-    @IsNotEmpty({ message: 'Description is required.' })
-    @MinLength(2, { message: 'Description must be at least 2 characters.' })
-    @MaxLength(255, { message: 'Description must be at most 255 characters.' })
-    @IsString({ message: 'Description must be in string format.' })
-    description: string; 
+  @ApiPropertyOptional()
+  @IsOptional()
+  @Transform(({ value }) => trim(value))
+  website?: string;
 
-    @ApiPropertyOptional()
-    @IsOptional()
-    @Transform(({ value }) => trim(value))
-    website?: string;
-    
-    @ApiPropertyOptional()
-    @IsOptional()
-    @Transform(({ value }) => trim(value))
-    @IsNotEmpty({ message: 'notificationWebhook is required.' })
-    @IsString({ message: 'notificationWebhook must be in string format.' })
-    @IsUrl({
-        // eslint-disable-next-line camelcase
-        require_protocol: true, // require URL protocol (e.g., http:// or https://)
-        // eslint-disable-next-line camelcase
-        require_tld: true // require top-level domain (e.g., .com, .net)
+  @ApiPropertyOptional()
+  @IsOptional()
+  @Transform(({ value }) => trim(value))
+  @IsNotEmpty({ message: 'notificationWebhook is required.' })
+  @IsString({ message: 'notificationWebhook must be in string format.' })
+  @IsUrl({
+    // eslint-disable-next-line camelcase
+    require_protocol: true, // require URL protocol (e.g., http:// or https://)
+    // eslint-disable-next-line camelcase
+    require_tld: true // require top-level domain (e.g., .com, .net)
+  })
+  notificationWebhook?: string;
 
-    })
-    notificationWebhook?: string;
-    
-    @ApiPropertyOptional()
-    @IsOptional()
-    @Transform(({ value }) => trim(value))
-    @IsString({ message: 'logo must be in string format.' })
-    logo?: string = '';   
+  @ApiPropertyOptional()
+  @IsOptional()
+  @Transform(({ value }) => trim(value))
+  @IsString({ message: 'logo must be in string format.' })
+  logo?: string = '';
+
+  @ApiProperty({ example: '' })
+  @IsOptional()
+  @IsString()
+  registrationNumber?: string;
+
+  @ApiProperty({ example: '' })
+  @IsNotEmpty()
+  @IsString()
+  country?: string;
+
+  @ApiProperty({ example: '' })
+  @IsString()
+  @IsOptional()
+  state?: string;
+
+  @ApiProperty({ example: '' })
+  @IsOptional()
+  @IsString()
+  city?: string;
 }

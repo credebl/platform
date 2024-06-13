8cb4f89804cae000188d5b605fbe3279caaa12a5
pranalidhanavade <137780597+pranalidhanavade@users.noreply.github.com>
2024-06-13 19:17:13 +0530

    fix: network type changes (#771)
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>

--- apps/agent-service/src/agent-service.service.ts
+++ apps/agent-service/src/agent-service.service.ts
@@ -1065,7 +1065,7 @@ export class AgentServiceService {
       await this.agentServiceRepository.setPrimaryDid(storeDidDetails.did, orgId, storeDidDetails.didDocument);
     }
 
-    if (method === DidMethod.INDY) {
+    if (network) {
       const getLedgerDetails = await this.agentServiceRepository.getLedgerByNameSpace(network);
       await this.agentServiceRepository.updateLedgerId(orgId, getLedgerDetails.id);
     }

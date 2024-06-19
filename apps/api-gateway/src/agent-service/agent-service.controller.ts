daea9b9ba58293e4d666f858e0a7946d7d43e3a0
KulkarniShashank <shashank.kulkarni@ayanworks.com>
2024-06-19 17:21:01 +0530

    fix: added the trim pipeline for orgId in organization and agent service
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/api-gateway/src/agent-service/agent-service.controller.ts
+++ apps/api-gateway/src/agent-service/agent-service.controller.ts
@@ -44,7 +44,7 @@ import { OrgRolesGuard } from '../authz/guards/org-roles.guard';
 import { validateDid } from '@credebl/common/did.validator';
 import { CreateWalletDto } from './dto/create-wallet.dto';
 import { CreateNewDidDto } from './dto/create-new-did.dto';
-import { AgentSpinupValidator } from '@credebl/common/cast.helper';
+import { AgentSpinupValidator, TrimStringParamPipe } from '@credebl/common/cast.helper';
 import { AgentConfigureDto } from './dto/agent-configure.dto';
 
 const seedLength = 32;
@@ -308,7 +308,7 @@ export class AgentController {
   @Roles(OrgRoles.OWNER)
   @ApiResponse({ status: HttpStatus.OK, description: 'Success', type: ApiResponseDto })
   async deleteWallet(
-    @Param('orgId', new ParseUUIDPipe({exceptionFactory: (): Error => { throw new BadRequestException(ResponseMessages.organisation.error.invalidOrgId); }})) orgId: string, 
+    @Param('orgId', TrimStringParamPipe, new ParseUUIDPipe({exceptionFactory: (): Error => { throw new BadRequestException(ResponseMessages.organisation.error.invalidOrgId); }})) orgId: string, 
     @User() user: user,
     @Res() res: Response
   ): Promise<Response> {

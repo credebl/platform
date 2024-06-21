c0255d40f6633554c42019eeea0b2170da557f36
bhavanakarwade <bhavana.karwade@ayanworks.com>
2024-06-21 20:26:30 +0530

    feat: support nats queue
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>

--- libs/common/src/nats.config.ts
+++ libs/common/src/nats.config.ts
@@ -2,7 +2,7 @@ import { NATSReconnects } from '@credebl/enum/enum';
 import { Authenticator, nkeyAuthenticator } from 'nats';
 
 export const getNatsOptions = (
-  nkeySeed?: string, serviceName?: string
+  nkeySeed: string, serviceName: string
 ): {
   servers: string[];
   authenticator?: Authenticator;
@@ -14,7 +14,6 @@ export const getNatsOptions = (
     servers: `${process.env.NATS_URL}`.split(','),
     maxReconnectAttempts: NATSReconnects.maxReconnectAttempts,
     reconnectTimeWait: NATSReconnects.reconnectTimeWait,
-    // queue: `${CommonConstants.API_GATEWAY_SERVICE}`
     queue: serviceName
   };
 
@@ -22,18 +21,8 @@ export const getNatsOptions = (
     return {
       ...baseOptions,
       authenticator: nkeyAuthenticator(new TextEncoder().encode(nkeySeed))
-      // queue: `${CommonConstants.API_GATEWAY_SERVICE}`
     };
   } 
-  // else if (nkeySeed && serviceName) {
-  //   return {
-  //     ...baseOptions,
-  //     authenticator: nkeyAuthenticator(new TextEncoder().encode(nkeySeed)),
-  //     queue: serviceName
-  //   };
-
-  // }
-
   return baseOptions;
 };
 

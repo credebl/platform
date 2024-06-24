0ba79500e4f984cf2639126ded27728475614082
bhavanakarwade <137506897+bhavanakarwade@users.noreply.github.com>
2024-06-24 19:40:05 +0530

    Feat/support multiple nats url (#805)
    * wip: nats configuration
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    
    * refactor: nats config function
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    
    * wip: implemented nats queue functionality
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    
    * wip: implemented nats queue functionality
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    
    * wip: nats queue configurations
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    
    * feat: support nats queue
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    
    * fix: add logger
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    
    * refactor: added logger
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    
    * fix: remove unnecessary logs
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    
    * fix: added optional parameter
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    
    * feat: implement nats queue
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    
    * refactor: added geo-service queue
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    
    ---------
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/geo-location/src/main.ts
+++ apps/geo-location/src/main.ts
@@ -4,13 +4,14 @@ import { NestFactory } from '@nestjs/core';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { getNatsOptions } from '@credebl/common/nats.config';
 import { GeoLocationModule } from './geo-location.module';
+import { CommonConstants } from '@credebl/common/common.constant';
 
 const logger = new Logger();
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(GeoLocationModule, {
     transport: Transport.NATS,
-    options: getNatsOptions(process.env.GEOLOCATION_NKEY_SEED)
+    options: getNatsOptions(CommonConstants.GEO_LOCATION_SERVICE, process.env.GEOLOCATION_NKEY_SEED)
   });
   app.useGlobalFilters(new HttpExceptionFilter());
 

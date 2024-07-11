3f500b09d2b14357d659d2abb5e86b30d22c4902
bhavanakarwade <bhavana.karwade@ayanworks.com>
2024-07-11 11:12:36 +0530

    fix: create organization bug
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>

--- libs/common/src/cast.helper.ts
+++ libs/common/src/cast.helper.ts
@@ -90,13 +90,19 @@ export const IsNotSQLInjection =
         validate(value) {
           // Check if the value contains any common SQL injection keywords
           const sqlKeywords = ['SELECT', 'INSERT', 'UPDATE', 'DELETE', 'DROP', 'UNION', 'WHERE', 'AND', 'OR'];
+        if ('string' === typeof value) {
+          // Convert the value to upper case for case-insensitive comparison
+          const upperCaseValue = value.toUpperCase();
+          // Use a regular expression to check for whole words
           for (const keyword of sqlKeywords) {
-            if (value?.includes(keyword)) {
+            const regex = new RegExp(`\\b${keyword}\\b`, 'i');
+            if (regex.test(upperCaseValue)) {
               return false; // Value contains a SQL injection keyword
             }
           }
-          return true; // Value does not contain any SQL injection keywords
-        },
+        }
+        return true; // Value does not contain any SQL injection keywords
+      },
         defaultMessage(args: ValidationArguments) {
           return `${args.property} contains SQL injection keywords.`;
         }

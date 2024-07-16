e43829fd1e05fda75352e5b36054e232507eafe3
Ganesh Nawle <ganesh.nawle@ayanworks.com>
2024-07-16 18:22:58 +0530

    fix:removed all console logs
    Signed-off-by: Ganesh Nawle <ganesh.nawle@ayanworks.com>

--- apps/issuance/src/issuance.service.ts
+++ apps/issuance/src/issuance.service.ts
@@ -798,7 +798,6 @@ async sendEmailForCredentialOffer(sendEmailCredentialOffer: SendEmailCredentialO
         const qrCodeOptions = { type: 'image/png' };
         const outOfBandIssuanceQrCode = await QRCode.toDataURL(shortenUrl, qrCodeOptions);
         const platformConfigData = await this.issuanceRepository.getPlatformConfigDetails();
-        this.logger.log(`Inside Payload 5 :::::${JSON.stringify(platformConfigData)}`);
         if (!platformConfigData) {
           errors.push(new NotFoundException(ResponseMessages.issuance.error.platformConfigNotFound));
           return false;
@@ -817,7 +816,6 @@ async sendEmailForCredentialOffer(sendEmailCredentialOffer: SendEmailCredentialO
           }
         ];
 
-        this.logger.log(`Inside sendEmailForCredentialOffer email data payload is::: ${JSON.stringify(this.emailData)}`);
 
         const isEmailSent = await sendEmail(this.emailData);   
          

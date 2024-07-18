d46473bfc4d16cd29f18f1816c93985c2eff6012
KulkarniShashank <shashank.kulkarni@ayanworks.com>
2024-07-18 16:29:45 +0530

    feat: configure cloud base wallet
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/cloud-wallet/src/cloud-wallet.controller.ts
+++ apps/cloud-wallet/src/cloud-wallet.controller.ts
@@ -3,11 +3,16 @@ import { Controller } from '@nestjs/common'; // Import the common service in the
 import { CloudWalletService } from './cloud-wallet.service'; // Import the common service in connection module
 import { MessagePattern } from '@nestjs/microservices'; // Import the nestjs microservices package
 import { ICreateCloudWallet, IStoredWalletDetails } from '@credebl/common/interfaces/cloud-wallet.interface';
+import { IConfigureCloudBaseWalletPayload, IGetStoredWalletInfo } from '../interfaces/cloud-wallet.interface';
 
 @Controller()
 export class CloudWalletController {
   constructor(private readonly cloudWalletService: CloudWalletService) {}
 
+  @MessagePattern({ cmd: 'configure-cloud-base-wallet' })
+  async configureBaseWallet(payload: IConfigureCloudBaseWalletPayload): Promise<IGetStoredWalletInfo> {
+    return this.cloudWalletService.configureBaseWallet(payload.cloudBaseWalletConfigure, payload.user);
+  }
 
   @MessagePattern({ cmd: 'create-cloud-wallet' })
   async createConnectionInvitation(cloudWalletDetails: ICreateCloudWallet): Promise<IStoredWalletDetails> {

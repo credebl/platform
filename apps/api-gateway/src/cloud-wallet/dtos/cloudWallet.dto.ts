3941f78d46db35828b37f41c2e7d123a4e3e8204
Shashank Kulkarni <44693969+KulkarniShashank@users.noreply.github.com>
2024-07-19 17:49:26 +0530

    feat: implement proof presentation for holder (#860)
    * feat: accept proof request by holder API
    
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>
    
    * feat: holder get proof presentation and get proof presentation by id
    
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>
    
    * refactor: changes in accept proof request and get proof request
    
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>
    
    ---------
    
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/api-gateway/src/cloud-wallet/dtos/cloudWallet.dto.ts
+++ apps/api-gateway/src/cloud-wallet/dtos/cloudWallet.dto.ts
@@ -16,5 +16,4 @@ export class CreateCloudWalletDto {
     @IsNotEmpty({ message: 'please provide valid image URL' })
     @IsNotSQLInjection({ message: 'Image URL is required.' })
     connectionImageUrl?: string;
-
 }

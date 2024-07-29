58942d8df1b58a3e35ef0053aa6e8320f3b49d37
bhavanakarwade <bhavana.karwade@ayanworks.com>
2024-07-29 18:11:07 +0530

    refactor: added comments for differentiate credential format
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/verification/src/verification.service.ts
+++ apps/verification/src/verification.service.ts
@@ -725,6 +725,7 @@ export class VerificationService {
 
       const extractedDataArray: IProofPresentationDetails[] = [];
 
+      // For Presentation exchange format
       if (getProofPresentationById?.response?.request?.presentationExchange) {
         const requestedAttributesForPresentationExchangeFormat =
           getProofPresentationById?.response?.request?.presentationExchange?.presentation_definition
@@ -757,12 +758,12 @@ export class VerificationService {
         }
       }
 
+      // For Indy format
       if (getProofPresentationById?.response?.request?.indy) {
-
         const requestedAttributes = getProofPresentationById?.response?.request?.indy?.requested_attributes;
         const requestedPredicates = getProofPresentationById?.response?.request?.indy?.requested_predicates;
         const revealedAttrs = getProofPresentationById?.response?.presentation?.indy?.requested_proof?.revealed_attrs;
-    
+
         if (0 !== Object.keys(requestedAttributes).length && 0 !== Object.keys(requestedPredicates).length) {
           for (const key in requestedAttributes) {
 

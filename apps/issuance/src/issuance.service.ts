c9e7ddd37d1258b2423ac1d4d0ef89fa370bcaad
bhavanakarwade <137506897+bhavanakarwade@users.noreply.github.com>
2024-08-16 17:41:19 +0530

    refactor: oob-issuance function (#917)
    * wip: fetch webhook url
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    
    * wip: get webhook url
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    
    * refactor: fetch verification list function
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    
    * refactor: support date-time datatype while schema creation
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    
    * rrsolved sonalint issues
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    
    * fix: resolved sonar issues
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    
    * refactor: apply validations for current date while issuance
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    
    * fix: verification list changes
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    
    * fix: date-time issue
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    
    ---------
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    Signed-off-by: KulkarniShashank <shashank.kulkarni@ayanworks.com>

--- apps/issuance/src/issuance.service.ts
+++ apps/issuance/src/issuance.service.ts
@@ -553,7 +553,9 @@ async outOfBandCredentialOffer(outOfBandCredential: OutOfBandCredentialOfferPayl
       credentialType
     } = outOfBandCredential;
 
-    await validateAndUpdateIssuanceDates(credentialOffer);
+    if (IssueCredentialType.JSONLD === credentialType) {
+      await validateAndUpdateIssuanceDates(credentialOffer);
+    }
 
     if (IssueCredentialType.INDY === credentialType) {  
       const schemaResponse: SchemaDetails = await this.issuanceRepository.getCredentialDefinitionDetails(

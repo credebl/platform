0e03febd6c45f34467ecce866705c544e64a3392
Tipu_Singh <tipu.singh@ayanworks.com>
2024-10-14 18:29:35 +0530

    fix: schema creation logic for mainnet
    Signed-off-by: Tipu_Singh <tipu.singh@ayanworks.com>

--- libs/common/src/common.constant.ts
+++ libs/common/src/common.constant.ts
@@ -157,6 +157,11 @@ export enum CommonConstants {
   // EVENTS
   EVENT_AUDIT = 'audit_event',
 
+  // POLYGON KEYWORDS
+  POLYGON = 'polygon',
+  POLYGON_MAINNET = 'mainnet',
+  POLYGON_TESTNET = 'testnet',
+
   // DOMAIN EVENTS
   DOMAIN_EVENT_SCHEMA_CREATED = 'Schema Created',
   DOMAIN_EVENT_CRED_DEF_CREATED = 'Cred-Def Created',

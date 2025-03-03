ba4f05f0f6675bf55cfb0c8f74a7d8c860ee4362
pranalidhanavade <pranali.dhanavade@ayanworks.com>
2025-03-03 10:23:54 +0530

    fix: added API params validations
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>

--- apps/api-gateway/src/agent-service/agent-service.controller.ts
+++ apps/api-gateway/src/agent-service/agent-service.controller.ts
@@ -71,8 +71,10 @@ export class AgentController {
     summary: 'Get the agent health details',
     description: 'Get the agent health details for the organization'
   })
-  @UseGuards(AuthGuard('jwt'))
-  async getAgentHealth(@Param('orgId') orgId: string, @User() reqUser: user, @Res() res: Response): Promise<Response> {
+  @UseGuards(AuthGuard('jwt'), OrgRolesGuard)
+  @Roles(OrgRoles.OWNER, OrgRoles.ADMIN, OrgRoles.HOLDER, OrgRoles.ISSUER, OrgRoles.SUPER_ADMIN, OrgRoles.SUPER_ADMIN, OrgRoles.MEMBER, OrgRoles.VERIFIER)
+  
+  async getAgentHealth(@Param('orgId') orgId: string, @User() reqUser: user, @Res() res: Response): Promise<Response> {    
     const agentData = await this.agentService.getAgentHealth(reqUser, orgId);
 
     const finalResponse: IResponse = {

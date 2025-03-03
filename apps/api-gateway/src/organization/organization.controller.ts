017e901c1a1560e6a18db6d1b02cebb9fec1c6db
bhavanakarwade <bhavana.karwade@ayanworks.com>
2025-03-03 12:04:03 +0530

    fix: added api param validations (#1121)
    * fix: added api param validations
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    
    * fix: removed unnecessary validations
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    
    ---------
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>

--- apps/api-gateway/src/organization/organization.controller.ts
+++ apps/api-gateway/src/organization/organization.controller.ts
@@ -109,8 +109,12 @@ export class OrganizationController {
   })
   @ApiResponse({ status: HttpStatus.OK, description: 'Success', type: ApiResponseDto })
   @UseGuards(AuthGuard('jwt'))
+  @Roles(OrgRoles.OWNER, OrgRoles.ADMIN)
   @ApiBearerAuth()
-  async getOrgRoles(@Param('orgId', new ParseUUIDPipe({exceptionFactory: (): Error => { throw new BadRequestException(ResponseMessages.organisation.error.invalidOrgId); }})) orgId: string, @User() user: user, @Res() res: Response): Promise<Response> {
+  async getOrgRoles(
+    @Param('orgId') orgId: string, 
+    @User() user: user, 
+    @Res() res: Response): Promise<Response> {
 
     const orgRoles = await this.organizationService.getOrgRoles(orgId.trim(), user);
 

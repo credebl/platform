c2c88e0f0f05464e09f4c7812dcafbc1a2024deb
bhavanakarwade <bhavana.karwade@ayanworks.com>
2025-03-20 10:53:25 +0530

    fix: Parameter validations (#1138)
    * fix: added api param validations
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    
    * fix: removed unnecessary validations
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    
    * fix:resolved validations issue
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    
    * added comment on function for understanding
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    
    * fix: resolve orgid validations
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    
    * fix: added response message
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    
    * fix: added space in response messages
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    
    ---------
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    Signed-off-by: Ankita Patidar <ankita.patidar@ayanworks.com>

--- apps/api-gateway/src/credential-definition/dto/create-cred-defs.dto.ts
+++ apps/api-gateway/src/credential-definition/dto/create-cred-defs.dto.ts
@@ -1,18 +1,20 @@
 import { IsBoolean, IsDefined, IsNotEmpty, IsOptional, IsString } from 'class-validator';
-
 import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import { trim } from '@credebl/common/cast.helper';
 
 export class CreateCredentialDefinitionDto {
 
     @ApiProperty({ 'example': 'default' })
-    @IsDefined({ message: 'Tag is required.' })
+    @IsDefined({ message: 'Tag is required' })
     @IsNotEmpty({ message: 'Please provide a tag' })
-    @IsString({ message: 'Tag id should be string' })
+    @IsString({ message: 'Tag should be string' })
     tag: string;
 
     @ApiProperty({ 'example': 'WgWxqztrNooG92RXvxSTWv:2:schema_name:1.0' })
-    @IsDefined({ message: 'schemaLedgerId is required.' })
-    @IsNotEmpty({ message: 'Please provide a schema id' })
+    @IsDefined({ message: 'schemaLedgerId is required' })
+    @IsNotEmpty({ message: 'Please provide valid schema ledger Id' })
+    @Transform(({ value }) => trim(value))
     @IsString({ message: 'Schema id should be string' })
     schemaLedgerId: string;
 

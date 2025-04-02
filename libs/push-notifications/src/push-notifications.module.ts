5448b6de2c1679b043aae8e871fbc07772f37768
Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
2025-04-02 14:27:10 +0530

    chore: remove unused modules (#1163)
    Signed-off-by: Sahil Kamble <sahil.kamble@ayanworks.com>

--- libs/push-notifications/src/push-notifications.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PushNotificationsService } from './push-notifications.service';
-
-@Module({
-  providers: [PushNotificationsService],
-  exports: [PushNotificationsService]
-})
-export class PushNotificationsModule {}

684ab5a190e116bac2210130ab2c87eead70bf8e
Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
2025-04-04 18:33:27 +0530

    refactor: create common package in libs (#1167)
    * refactor: merge nats-interceptor into common
    
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
    
    * refactor: merge repsonses lib into common
    
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
    
    * refactor: create common function to handle errors
    
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
    
    * chore: add todo
    
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
    
    * refactor: create common package in libs
    
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
    
    * fix: remove missed image service
    
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
    
    * refactor: common service
    
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
    
    ---------
    
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
    Signed-off-by: Sahil Kamble <sahil.kamble@ayanworks.com>

--- libs/common/src/interfaces/verification.interface.ts
+++ libs/common/src/interfaces/verification.interface.ts
@@ -1,51 +1,62 @@
-import { Prisma } from "@prisma/client";
+import { Prisma } from '@prisma/client';
 
 export interface IProofPresentationsListCount {
-    proofRequestsCount: number;
-    proofRequestsList: IProofPresentationItem[];
-  }
-  export interface IProofPresentationItem {
-    id: string,
-    createDateTime: Date;
-    createdBy: string;
-    connectionId: string;
-    state: string;
-    orgId: string;
-    presentationId: string; 
-    schemaId?: string; 
-    emailId?: string
-  }
-  export interface IProofPresentationList {
-    totalItems: number;
-    hasNextPage: boolean;
-    hasPreviousPage: boolean;
-    nextPage: number;
-    previousPage: number;
-    lastPage: number;
-    data: IProofPresentationItem[];
-  }
+  proofRequestsCount: number;
+  proofRequestsList: IProofPresentationItem[];
+}
+export interface IProofPresentationItem {
+  id: string;
+  createDateTime: Date;
+  createdBy: string;
+  connectionId: string;
+  state: string;
+  orgId: string;
+  presentationId: string;
+  schemaId?: string;
+  emailId?: string;
+}
+export interface IProofPresentationList {
+  totalItems: number;
+  hasNextPage: boolean;
+  hasPreviousPage: boolean;
+  nextPage: number;
+  previousPage: number;
+  lastPage: number;
+  data: IProofPresentationItem[];
+}
 
-  export interface IProofPresentationDetails {
-    [key: string]: string;
-    credDefId?: string;
-    schemaId: string;
-    certificateTemplate?: string;
-  }
-  export interface IVerificationRecords {
-    deleteResult: Prisma.BatchPayload;
-    recordsToDelete: IRecords[]
-  }
+export interface IProofPresentationDetails {
+  [key: string]: string;
+  credDefId?: string;
+  schemaId: string;
+  certificateTemplate?: string;
+}
+export interface IVerificationRecords {
+  deleteResult: Prisma.BatchPayload;
+  recordsToDelete: IRecords[];
+}
 
 interface IRecords {
-    id: string;
-    createDateTime: Date;
-    createdBy: string;
-    lastChangedDateTime: Date;
-    lastChangedBy: string;
-    connectionId: string;
-    orgId: string;
-    presentationId: string;
-    isVerified: boolean;
-    threadId: string;
-    state: string;
+  id: string;
+  createDateTime: Date;
+  createdBy: string;
+  lastChangedDateTime: Date;
+  lastChangedBy: string;
+  connectionId: string;
+  orgId: string;
+  presentationId: string;
+  isVerified: boolean;
+  threadId: string;
+  state: string;
+}
+
+// TODO: Need to check if this is a duplicate
+export interface IProofRequestAttribute {
+  attributeName?: string;
+  attributeNames?: string[];
+  condition?: string;
+  value?: string;
+  credDefId?: string;
+  schemaId?: string;
+  credentialName?: string;
 }

dd2dc0e202aebe3a4e4c4befc5c6ea571ec845b4
rockito10 <rockito10.mfd@gmail.com>
2025-04-10 16:35:39 -0300

    Change validator, ease syntax
    Signed-off-by: rockito10 <rockito10.mfd@gmail.com>

---
+++ config/validation/error.ts
@@ -0,0 +1,45 @@
+type Primitive = string | number | null | undefined;
+
+export type Issue = {
+  path?: string;
+  expected?: Primitive;
+  received?: Primitive;
+  message: string;
+};
+
+type FlattenedIssue = { [key: string]: string[] };
+
+// ----------
+
+export class VError extends Error {
+  private issues: Issue[];
+
+  constructor(errors: Issue[]) {
+    const message = JSON.stringify(errors, null, 2);
+
+    super(message);
+    this.issues = errors;
+    this.name = 'VError';
+  }
+
+  public errors(): Issue[] {
+    return this.issues;
+  }
+
+  public flatten(): FlattenedIssue {
+    return this.errors().reduce((flattened, error) => {
+      const key = error.path || 'messages';
+
+      if (!flattened[key]) {
+        flattened[key] = [];
+      }
+
+      flattened[key].push({
+        message: error.message,
+        received: error.received
+      });
+
+      return flattened;
+    }, {});
+  }
+}

f2dae485ed843b0ebd6995d7a30db3c50665f5a7
Tipu_Singh <tipu.singh@ayanworks.com>
2025-06-24 14:16:59 +0530

    feat: otel and signoz implementation
    Signed-off-by: Tipu_Singh <tipu.singh@ayanworks.com>

--- libs/logger/src/nestjsLoggerServiceAdapter.ts
+++ libs/logger/src/nestjsLoggerServiceAdapter.ts
@@ -1,43 +1,53 @@
-import { ConsoleLogger } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { ConsoleLogger, Injectable, Scope } from '@nestjs/common';
 import Logger from '@credebl/logger/logger.interface';
 import { LoggerService } from '@nestjs/common/services/logger.service';
 import { LogData } from '@credebl/logger/log';
+import api from '@opentelemetry/api';
 
-export default class NestjsLoggerServiceAdapter
-  extends ConsoleLogger
-  implements LoggerService
-{
+export default class NestjsLoggerServiceAdapter extends ConsoleLogger implements LoggerService {
   public constructor(private readonly logger: Logger) {
     super();
   }
 
-  public info(message: any, ...optionalParams: any[]) : void {
-    return this.logger.info(message, this.getLogData(optionalParams));
+  public info(message: any, ...optionalParams: any[]): void {
+    this.logger.info(message, this.getLogData(optionalParams));
   }
 
-  public log(message: any, ...optionalParams: any[]) : void {
-    return this.logger.info(message, this.getLogData(optionalParams));
+  public log(message: any, ...optionalParams: any[]): void {
+    this.logger.info(message, this.getLogData(optionalParams));
   }
 
-  public error(message: any, ...optionalParams: any[]) : void {
-    return this.logger.error(message, this.getLogData(optionalParams));
+  public error(message: any, ...optionalParams: any[]): void {
+    this.logger.error(message, this.getLogData(optionalParams));
   }
 
-  public warn(message: any, ...optionalParams: any[]) : void {
-    return this.logger.warn(message, this.getLogData(optionalParams));
+  public warn(message: any, ...optionalParams: any[]): void {
+    this.logger.warn(message, this.getLogData(optionalParams));
   }
 
-  public debug(message: any, ...optionalParams: any[]) : void {
-    return this.logger.debug(message, this.getLogData(optionalParams));
+  public debug(message: any, ...optionalParams: any[]): void {
+    this.logger.debug(message, this.getLogData(optionalParams));
   }
 
-  public verbose(message: any, ...optionalParams: any[]) : void {
-    return this.logger.info(message, this.getLogData(optionalParams));
+  public verbose(message: any, ...optionalParams: any[]): void {
+    this.logger.info(message, this.getLogData(optionalParams));
   }
 
-  private getLogData(...optionalParams: any[]) : LogData  {
+  private getLogData(...optionalParams: any[]): LogData {
     return {
-      sourceClass: optionalParams[0] ? optionalParams[0] : undefined,
+      sourceClass: optionalParams[0] ? optionalParams[0] : undefined
     };
+  }
+}
+
+@Injectable({ scope: Scope.TRANSIENT })
+export class MyLoggerService extends ConsoleLogger {
+  customLog(message: string): void {
+    const activeSpan = api.trace.getSpan(api.context.active());
+    if (activeSpan) {
+      activeSpan.addEvent(message);
+    }
+    this.log(message);
   }
 }

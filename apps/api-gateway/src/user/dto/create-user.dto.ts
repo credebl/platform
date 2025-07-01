fca8aba8feb3a9098617cd215136bb56ba3200fe
bhavanakarwade <bhavana.karwade@ayanworks.com>
2025-07-01 17:17:38 +0530

    feat: single sign on (#1305)
    * feat: single sign on
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    
    * fix: rabbit suggestions
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    
    * update: send verification mail template
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>
    
    ---------
    
    Signed-off-by: bhavanakarwade <bhavana.karwade@ayanworks.com>

--- apps/api-gateway/src/user/dto/create-user.dto.ts
+++ apps/api-gateway/src/user/dto/create-user.dto.ts
@@ -1,41 +1,41 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsOptional, IsString, IsUrl, MaxLength } from 'class-validator';
 import { toLowerCase, trim } from '@credebl/common/cast.helper';
 
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
 
 export class UserEmailVerificationDto {
-    @ApiProperty({ example: 'awqx@yopmail.com' })
-    @Transform(({ value }) => trim(value))
-    @Transform(({ value }) => toLowerCase(value))
-    @IsNotEmpty({ message: 'Email is required.' })
-    @MaxLength(256, { message: 'Email must be at most 256 character.' })
-    @IsEmail({}, { message: 'Please provide a valid email' })
-    email: string;
+  @ApiProperty({ example: 'awqx@yopmail.com' })
+  @Transform(({ value }) => trim(value))
+  @Transform(({ value }) => toLowerCase(value))
+  @IsNotEmpty({ message: 'Email is required.' })
+  @MaxLength(256, { message: 'Email must be at most 256 character.' })
+  @IsEmail({}, { message: 'Please provide a valid email' })
+  email: string;
 
-    @ApiProperty({ example: 'xxxx-xxxx-xxxx' })
-    @IsString({ message: 'clientId should be string' })
-    clientId: string;
-
-    @ApiProperty({ example: 'xxxx-xxxxx-xxxxx' })
-    @IsString({ message: 'clientSecret should be string' })
-    clientSecret: string;
-
-    @ApiPropertyOptional({ example: 'https://example.com/logo.png' })
-    @Transform(({ value }) => trim(value))
-    @IsOptional()
-    @IsUrl({
-    // eslint-disable-next-line camelcase 
-        require_protocol: true,
-    // eslint-disable-next-line camelcase
+  @ApiPropertyOptional({ example: 'https://example.com/logo.png' })
+  @Transform(({ value }) => trim(value))
+  @IsOptional()
+  @IsUrl(
+    {
+      // eslint-disable-next-line camelcase
+      require_protocol: true,
+      // eslint-disable-next-line camelcase
       require_tld: true
     },
-    { message: 'brandLogoUrl should be a valid URL' })
-    brandLogoUrl?: string;
+    { message: 'brandLogoUrl should be a valid URL' }
+  )
+  brandLogoUrl?: string;
 
   @ApiPropertyOptional({ example: 'MyPlatform' })
   @Transform(({ value }) => trim(value))
   @IsOptional()
   @IsString({ message: 'platformName should be string' })
   platformName?: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString({ message: 'clientAlias should be string' })
+  @Transform(({ value }) => trim(value))
+  clientAlias?: string;
 }

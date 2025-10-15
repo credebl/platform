5e474cd192daca060d3551e85f289b4d3b250c86
Ankita Patidar <ankita.patidar@ayanworks.com>
2025-10-15 12:14:52 +0530

    fix: minimize data while getting connection
    Signed-off-by: Ankita Patidar <ankita.patidar@ayanworks.com>

--- apps/connection/src/connection.service.ts
+++ apps/connection/src/connection.service.ts
@@ -528,21 +528,16 @@ export class ConnectionService {
         throw new NotFoundException(ResponseMessages.connection.error.agentEndPointNotFound);
       }
 
-      let legacyinvitationDid;
+      let legacyInvitationDid;
       if (IsReuseConnection) {
-        const data: agent_invitations[] = await this.connectionRepository.getInvitationDidByOrgId(orgId);
-        if (data && 0 < data.length) {
-          const [firstElement] = data;
-          legacyinvitationDid = firstElement?.invitationDid ?? undefined;
-
-          this.logger.log('legacyinvitationDid:', legacyinvitationDid);
-        }
+        const data: agent_invitations = await this.connectionRepository.getInvitationDidByOrgId(orgId);
+        legacyInvitationDid = data.invitationDid ?? undefined;
       }
-      const connectionInvitationDid = invitationDid ? invitationDid : legacyinvitationDid;
+      const connectionInvitationDid = invitationDid ? invitationDid : legacyInvitationDid;
 
-      this.logger.log('connectionInvitationDid:', connectionInvitationDid);
+      this.logger.debug('connectionInvitationDid:', connectionInvitationDid);
 
-      this.logger.log(`logoUrl:::, ${organisation.logoUrl}`);
+      this.logger.debug(`logoUrl:::, ${organisation.logoUrl}`);
       const connectionPayload = {
         multiUseInvitation: multiUseInvitation ?? true,
         autoAcceptConnection: autoAcceptConnection ?? true,

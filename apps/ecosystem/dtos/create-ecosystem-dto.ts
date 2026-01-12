5621a8961901102973ec3344aea841bb8a55e634
pranalidhanavade <pranali.dhanavade@ayanworks.com>
2026-01-12 11:51:35 +0530

    feat: ecosystem service and create ecosystem invitation API route (#1540)
    * feat/add script to add platform admin keycloak and role
    
    Signed-off-by: sujitaw <sujit.sutar@ayanworks.com>
    
    * fix/eslint issue
    
    Signed-off-by: sujitaw <sujit.sutar@ayanworks.com>
    
    * fix/coderabbit comments
    
    Signed-off-by: sujitaw <sujit.sutar@ayanworks.com>
    
    * fix/changes to fetch value from db
    
    Signed-off-by: sujitaw <sujit.sutar@ayanworks.com>
    
    * feat: ecosystem service and create ecosystem invitation API route
    
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>
    
    * fix: coderabbit issues
    
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>
    
    * fix: coderabbit warnings
    
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>
    
    * feat: get all invitations api
    
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>
    
    * refactor: removed userId parameter from send invitation and get invitation api
    
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>
    
    * feat: create and get ecosystem api routes
    
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>
    
    * fix: coderabbit warning
    
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>
    
    * fix: coderabbit warnings and suggestions
    
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>
    
    * fix: coderabbit warnings resolved
    
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>
    
    * fix: coderabbit issues
    
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>
    
    * resolved comments on PR
    
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>
    
    * resolve comments on PR
    
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>
    
    * resolve comments on PR
    
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>
    
    ---------
    
    Signed-off-by: sujitaw <sujit.sutar@ayanworks.com>
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>
    Co-authored-by: sujitaw <sujit.sutar@ayanworks.com>
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>

---
+++ apps/ecosystem/dtos/create-ecosystem-dto.ts
@@ -0,0 +1,42 @@
+import { ApiExtraModels, ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
+import { IsNotSQLInjection, trim } from '@credebl/common/cast.helper';
+import { Transform, Type } from 'class-transformer';
+
+@ApiExtraModels()
+export class CreateEcosystemDto {
+  @ApiProperty()
+  @Transform(({ value }) => trim(value))
+  @IsNotEmpty({ message: 'Ecosystem name is required.' })
+  @MinLength(2, { message: 'Ecosystem name must be at least 2 characters.' })
+  @MaxLength(50, { message: 'Ecosystem name must be at most 50 characters.' })
+  @IsString({ message: 'Ecosystem name must be in string format.' })
+  @IsNotSQLInjection({ message: 'Incorrect pattern for ecosystem name.' })
+  name: string;
+
+  @ApiProperty()
+  @Transform(({ value }) => trim(value))
+  @IsNotEmpty({ message: 'Description is required.' })
+  @MinLength(2, { message: 'Description must be at least 2 characters.' })
+  @MaxLength(255, { message: 'Description must be at most 255 characters.' })
+  @IsString({ message: 'Description must be in string format.' })
+  @IsNotSQLInjection({ message: 'Incorrect pattern for description.' })
+  description: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @Transform(({ value }) => trim(value))
+  @IsString({ message: 'tag must be in string format.' })
+  @Type(() => String)
+  tags?: string;
+
+  userId: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @Transform(({ value }) => trim(value))
+  @IsString({ message: 'logo must be in string format.' })
+  logo?: string;
+
+  orgId?: string;
+}

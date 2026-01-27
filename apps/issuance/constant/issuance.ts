0e13a81f60924a61a4fbc8a92819256b97435795
shitrerohit <rohit.shitre@ayanworks.com>
2026-01-27 12:05:12 +0530

    fix:removed unused variables
    Signed-off-by: shitrerohit <rohit.shitre@ayanworks.com>

--- apps/issuance/constant/issuance.ts
+++ apps/issuance/constant/issuance.ts
@@ -1,6 +1,3 @@
-import { AccessTokenSignerKeyType } from '../interfaces/oid4vc-issuance.interfaces';
-
 export const dpopSigningAlgValuesSupported = ['RS256', 'ES256', 'EdDSA'];
 export const credentialConfigurationsSupported = {};
-export const accessTokenSignerKeyType = 'Ed25519' as AccessTokenSignerKeyType;
 export const batchCredentialIssuanceDefault = 0;

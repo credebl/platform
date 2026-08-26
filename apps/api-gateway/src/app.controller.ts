bae7cb5249a912daee246cdd40dc74e5c3abb3b9
João  Victor B Miraya <108498460+joaoMiraya@users.noreply.github.com>
2026-08-26 09:15:16 -0300

    fix: redirect swagger fixed (#1572)
    * fix: redirect swagger fixed
    
    Signed-off-by: joaoMiraya <joaomiraya01@gmail.com>
    
    * fix: set env var to expose swagger endpoint
    
    Signed-off-by: joaoMiraya <joaomiraya01@gmail.com>
    
    * fix: fix redirect status and bool check
    
    Signed-off-by: joaoMiraya <joaomiraya01@gmail.com>
    
    * chore: add env var on .env.demo
    
    Signed-off-by: joaoMiraya <joaomiraya01@gmail.com>
    
    ---------
    
    Signed-off-by: joaoMiraya <joaomiraya01@gmail.com>
    Signed-off-by: João  Victor B Miraya <108498460+joaoMiraya@users.noreply.github.com>

--- apps/api-gateway/src/app.controller.ts
+++ apps/api-gateway/src/app.controller.ts
@@ -1,10 +1,26 @@
-import { Controller, Logger } from '@nestjs/common';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { Controller, Get, Logger, Redirect } from '@nestjs/common';
+import { ApiBearerAuth, ApiExcludeEndpoint } from '@nestjs/swagger';
 import { AppService } from './app.service';
+import { ConfigService } from '@credebl/config/config.service';
+
 @Controller()
 @ApiBearerAuth()
-export class AppController { 
-    constructor(private readonly appService: AppService) {}
+export class AppController {
+  constructor(
+    private readonly appService: AppService,
+    private readonly configService: ConfigService
+  ) {}
 
-    private readonly logger = new Logger('AppController');
+  private readonly logger = new Logger('AppController');
+
+  @Get()
+  @Redirect('/api', 302)
+  @ApiExcludeEndpoint()
+  redirectToSwagger(): { url?: string; statusCode: number } {
+    if (this.configService.isSwaggerAvailable) {
+      return { url: '/api', statusCode: 302 };
+    }
+
+    return { statusCode: 200 };
+  }
 }
